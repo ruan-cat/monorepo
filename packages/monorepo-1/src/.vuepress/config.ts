087ba2a90c067674c746a3f5dcd968eee0992887
ruan-cat <1219043956@qq.com>
2024-05-05 16:50:32 +0800

    ✨ feat: 删除多余重复的配置文件；安装局部依赖，并且使用包；

--- packages/monorepo-1/src/.vuepress/config.ts
+++ packages/monorepo-1/src/.vuepress/config.ts
@@ -1,21 +1,3 @@
-import { defineUserConfig } from "vuepress";
-import { viteBundler } from "@vuepress/bundler-vite";
-import theme from "./theme.js";
+import { defineRuanCatVuepressPresetConfig } from "@ruan-cat-vercel-monorepo-test/vuepress-preset-config";
 
-export default defineUserConfig({
-  base: "/",
-
-  lang: "zh-CN",
-  title: "文档演示",
-  description: "vuepress-theme-hope 的文档演示",
-
-  theme,
-
-  bundler: viteBundler({
-    viteOptions: {},
-    vuePluginOptions: {},
-  }),
-
-  // 和 PWA 一起启用
-  // shouldPrefetch: false,
-});
+export default defineRuanCatVuepressPresetConfig;
