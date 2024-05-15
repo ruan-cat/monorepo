25118ae40368c3238966ffbad9a189604c2ae12c
ruan-cat <1219043956@qq.com>
2024-05-15 23:12:55 +0800

    ⚙️ config: 正在学习 antfu ；

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,28 @@
 // @ts-check
-import antfu from '@antfu/eslint-config'
+import antfu from "@antfu/eslint-config";
 
-export default antfu()
+export default antfu({
+  // Enable stylistic formatting rules
+  // stylistic: true,
+
+  // Or customize the stylistic rules
+  stylistic: {
+    indent: 2, // 4, or 'tab'
+    quotes: "double", // or 'double'
+    semi: true, // or 'always'
+  },
+
+  // TypeScript and Vue are auto-detected, you can also explicitly enable them:
+  typescript: true,
+  vue: true,
+
+  // Disable jsonc and yaml support
+  jsonc: false,
+  yaml: false,
+
+  // `.eslintignore` is no longer supported in Flat config, use `ignores` instead
+  ignores: [
+    "**/fixtures",
+    // ...globs
+  ],
+});
