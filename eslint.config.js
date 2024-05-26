0e35ba6e86a1e19ec713f31dcd866414668e340b
ruan-cat <1219043956@qq.com>
2024-05-26 17:14:24 +0800

    🤔 save-file: 完成有效的eslint配置，eslint开始工作；

--- eslint.config.js
+++ eslint.config.js
@@ -1,72 +1,101 @@
 // @ts-check
-import antfu from "@antfu/eslint-config";
-import gitignore from "eslint-config-flat-gitignore";
+import antfu from "@antfu/eslint-config"
+import gitignore from "eslint-config-flat-gitignore"
+import js from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier";
+// const eslintConfigPrettier =   require("eslint-config-prettier");
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+import { FlatCompat } from "@eslint/eslintrc"
 
-export default antfu({
-  // Enable stylistic formatting rules
-  // stylistic: true,
+const compat = new FlatCompat({
+	recommendedConfig: js.configs.recommended,
+})
 
-  // Or customize the stylistic rules
-  stylistic: {
-    indent: 2, // 4, or 'tab'
-    quotes: "double", // or 'double'
-    semi: true, // or 'always'
-  },
+export default antfu(
+  {
+    // Enable stylistic formatting rules
+    // stylistic: true,
 
-  // TypeScript and Vue are auto-detected, you can also explicitly enable them:
-  typescript: {
-    tsconfigPath: "tsconfig.json",
-  },
-  vue: true,
+    // Or customize the stylistic rules
+    stylistic: {
+      indent: 2, // 4, or 'tab'
+      quotes: "double", // or 'double'
+      semi: true, // or 'always'
+    },
 
-  // Disable jsonc and yaml support
-  jsonc: false,
-  yaml: false,
+    // TypeScript and Vue are auto-detected, you can also explicitly enable them:
+    typescript: {
+      tsconfigPath: "tsconfig.json",
+    },
+    vue: true,
 
-  // `.eslintignore` is no longer supported in Flat config, use `ignores` instead
-  ignores: [
-    "**/fixtures",
-    // ...globs
-  ],
+    javascript: {
+      /**
+			 * @see https://eslint.org/docs/latest/use/configure/migration-guide#importing-plugins-and-custom-parsers
+			 */
+      overrides: { "jsdoc/require-description": "error", "jsdoc/check-values": "error" },
+    },
 
-  /**
-   * @see https://juejin.cn/post/7338074027281104936
-   */
-  formatters: {
+		// Disable jsonc and yaml support
+    jsonc: false,
+    yaml: false,
+
+    // `.eslintignore` is no longer supported in Flat config, use `ignores` instead
+    ignores: [
+      "**/fixtures",
+      // ...globs
+    ],
+
     /**
-     * Format CSS, LESS, SCSS files, also the `<style>` blocks in Vue
-     * By default uses Prettier
-     */
-    css: true,
-    /**
-     * Format HTML files
-     * By default uses Prettier
-     */
-    html: true,
-    /**
-     * Format Markdown files
-     * Supports Prettier and dprint
-     * By default uses Prettier
-     */
-    markdown: "prettier",
-  },
+		 * @see https://juejin.cn/post/7338074027281104936
+		 */
+    formatters: {
+      /**
+			 * Format CSS, LESS, SCSS files, also the `<style>` blocks in Vue
+			 * By default uses Prettier
+			 */
+      css: true,
+      /**
+			 * Format HTML files
+			 * By default uses Prettier
+			 */
+      html: true,
+      /**
+			 * Format Markdown files
+			 * Supports Prettier and dprint
+			 * By default uses Prettier
+			 */
+      markdown: "prettier",
+    },
+	},
 
-  javascript: {
-    /**
-     * @see https://eslint.org/docs/latest/use/configure/migration-guide#importing-plugins-and-custom-parsers
-     */
-    overrides: { "jsdoc/require-description": "error", "jsdoc/check-values": "error",
+  // Legacy config
+  ...compat.config({
+    extends: [
+      "eslint:recommended",
+			"prettier",
+      // Other extends...
+    ],
+  }),
+
+	/** https://github.com/antfu/eslint-config-flat-gitignore */
+  gitignore({
+    root: true,
+    strict: false,
+    files: [".gitignore", ".eslintignore"],
+  }),
+
+  {
+    rules: {
+      "no-console": "off",
+      "style/semi": ["error", "never"],
     },
   },
 
-},
-
-/** https://github.com/antfu/eslint-config-flat-gitignore */
-gitignore({
-  root: true,
-  strict: false,
-  files: [
-    ".gitignore",
-    ".eslintignore",
-  ],
-}));
+  // @ts-ignore
+  eslintConfigPrettier,
+  // {
+  // 	rules: eslintConfigPrettier
+  // },
+  eslintPluginPrettierRecommended,
+)
