bffa1ed37d59cf94552b5d9190c3b867486b859b
ruan-cat <1219043956@qq.com>
2024-05-30 20:27:20 +0800

    ⚙️ config: 应用最新的，eslint配置覆盖prettier的写法；

--- eslint.config.js
+++ eslint.config.js
@@ -73,6 +73,13 @@ export default antfu(
 				"error",
 				{
 					usePrettierrc: true,
+					singleQuote: false,
+					printWidth: 120,
+					semi: true,
+					jsxSingleQuote: true,
+					useTabs: true,
+					tabWidth: 2,
+					endOfLine: "auto",
 				},
 			],
 		},
