cef2c13e0ae1eae504d40357d21160a90a21447b
ruan-cat <1219043956@qq.com>
2024-08-30 17:18:52 +0800

    🤔 save-file: 初步入门 execa ，可以查询一大堆异步数据了。

--- demos/vercel-deploy-tool/src/index.ts
+++ demos/vercel-deploy-tool/src/index.ts
@@ -1,8 +1,20 @@
-// This is the entry point of the add-tool project
-// Implement the add-tool functionality here
+// 学习一下如何使用 https://github.com/sindresorhus/execa/blob/main/readme.md
+import { execa } from "execa";
+
+const pkgNames = <const>["pnpm", "turbopack", "vite", "vue", "koishi", "lodash", "axios"];
 
 export function add(a: number, b: number): number {
 	return a + b;
 }
 
-console.log(add(2, 3)); // Output: 5
+// console.log(add(2, 3)); // Output: 5
+
+// 查询多个包的版本
+const response = await Promise.all(
+	pkgNames.map((name) => {
+		return execa`pnpm v ${name}`;
+	}),
+);
+response.forEach((res) => {
+	console.log(" res.stdout ", res.stdout);
+});
