b8048237c83d69b99ca893d050da5ecad33dcf0a
ruan-cat <1219043956@qq.com>
2024-08-31 20:04:21 +0800

    ✨ feat: 增加注释；

--- demos/vercel-deploy-tool/src/index.ts
+++ demos/vercel-deploy-tool/src/index.ts
@@ -17,6 +17,7 @@ export {};
 // 	console.log(" res.stdout ", res.stdout);
 // });
 
+/** 当前的环境变量 */
 const currentDotenvConfig = dotenvConfig({
 	// 具体识别的路径，会自动识别根目录下面的env文件，故这里不作处理
 	//  path: "../../../.env"
@@ -24,40 +25,54 @@ const currentDotenvConfig = dotenvConfig({
 
 console.log(" 查看来自 @dotenvx/dotenvx 获取的环境变量： ", currentDotenvConfig);
 
+/** 项目配置 */
 interface Config {
+	/** 项目名称 */
 	vercelProjetName: string;
+
+	/** 用户token */
 	vercelToken: string;
+	/** 用户组织id */
 	vercelOrgId: string;
+	/** 用户项目id */
 	vercelProjectId: string;
+	/** 目标的工作目录 */
 	targetCWD: string;
-	url: string;
+	/** 生产环境的访问url */
+	url: string[];
+	/** 实际部署的构建命令 */
 	buildCommand: string[];
 }
 
+/** 项目内的vercel配置 */
 const config: Config = {
 	vercelProjetName: "vercel-monorepo-test-1-zn20",
-	vercelToken: "QF9Q3Hv5U8q2fKz1Jc4W8B1Y",
-	vercelOrgId: "QF9Q3Hv5U8q2fKz1Jc4W8B1Y",
-	vercelProjectId: "QF9Q3Hv5U8q2fKz1Jc4W8B1Y",
-	// targetCWD: "./packages/docs-01-star",
-	targetCWD: "packages/docs-01-star",
-	url: "docs-01-star.ruancat6312.top",
+	vercelToken: "",
+	vercelOrgId: "",
+	vercelProjectId: "",
+	targetCWD: "./packages/docs-01-star",
+	// targetCWD: "packages/docs-01-star",
+	url: ["docs-01-star.ruancat6312.top"],
 	buildCommand: [
+		// FIXME: 在具体的execa中，无法使用pnpm的筛选命令。只能指定其工作目录。
 		// "pnpm -F @ruan-cat-vercel-monorepo-test/docs-01-star build:docs",
 		// "pnpm -F @ruan-cat-vercel-monorepo-test/docs-01-star copy-dist",
-		"pnpm -C=packages/docs-01-star build:docs",
-		"pnpm -C=packages/docs-01-star copy-dist",
+		"pnpm -C=./packages/docs-01-star build:docs",
+		"pnpm -C=./packages/docs-01-star copy-dist",
 	],
 };
+// ('rimraf .vercel/output/static && mkdirp .vercel/output/static && cpx "docs/.vitepress/dist/**/*" .vercel/output/static && shx ls -R .vercel/output/static');
 
-// await execa`${config.buildCommand[0]}`;
-// await execa`pnpm run ${config.buildCommand[0]}`;
-// await execa`pnpm -v && pnpm -F @ruan-cat-vercel-monorepo-test/docs-01-star build:docs`;
-// const command = "pnpm -v";
-// const testRes = await execa`${config.buildCommand[0]}`;
-// console.log(" ? testRes  ", testRes.stdout);
-
-const vercelNullConfig = {
+/**
+ * vercel 的空配置
+ * @description
+ * 设计理由
+ *
+ * 用于驱动vercel构建简单的目录结构，不需要额外的配置
+ *
+ * 该配置会被写入到 `vercel.null.def.json` 文件中
+ */
+const vercelNullConfig = <const>{
 	framework: null,
 	buildCommand: null,
 	installCommand: null,
@@ -71,18 +86,32 @@ const vercelNullConfig = {
 	},
 };
 
+/**
+ * 空配置文件的路径
+ * @description
+ * 生成空配置文件。这样用户在其他项目内，就不需要自己提供vercel配置文件了。
+ */
 const vercelNullConfigPath = "./vercel.null.def.json";
 
-function initVercelNullConfig() {
+/** 初始化vercel的空配置文件 */
+function generateVercelNullConfig() {
 	fs.writeFileSync(vercelNullConfigPath, JSON.stringify(vercelNullConfig, null, 2));
 }
-initVercelNullConfig();
+generateVercelNullConfig();
 
-merge(config, {
-	vercelOrgId: process.env.VERCEL_ORG_ID,
-	vercelProjectId: process.env.VERCEL_PROJECT_ID,
-	vercelToken: process.env.VERCEL_TOKEN,
-} satisfies Partial<Config>);
+/**
+ * 初始化配置
+ * @description
+ * 初始化环境变量
+ */
+function initVercelConfig() {
+	merge(config, {
+		vercelOrgId: process.env.VERCEL_ORG_ID,
+		vercelProjectId: process.env.VERCEL_PROJECT_ID,
+		vercelToken: process.env.VERCEL_TOKEN,
+	} satisfies Partial<Config>);
+}
+initVercelConfig();
 
 function link() {
 	return execa`vc link --yes --cwd=${config.targetCWD} --project=${config.vercelProjetName} -t ${config.vercelToken}`;
@@ -118,3 +147,5 @@ for await (const buildCommand of buildCommands) {
 	const buildCommandStdout = await buildCommand();
 	console.log(" in buildCommandStdout ", buildCommandStdout.stdout);
 }
+
+// TODO: 实现 deploy 命令；
