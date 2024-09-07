cd1953e79e577ccb0a416f9bbf2a69ab929ed25d
ruan-cat <1219043956@qq.com>
2024-09-07 22:07:49 +0800

    refactor: 优化runPromiseByQueue函数
    简化runPromiseByQueue函数的实现，使用reduce方法实现Promise串行执行，提高代码可读性和性能。

--- demos/vercel-deploy-tool/src/utils/simple-promise-tools.ts
+++ demos/vercel-deploy-tool/src/utils/simple-promise-tools.ts
@@ -3,12 +3,10 @@
  * @see https://github.com/ascoders/weekly/blob/master/前沿技术/77.精读《用%20Reduce%20实现%20Promise%20串行执行》.md
  */
 function runPromiseByQueue<T>(promises: ((...args: any) => Promise<T>)[]) {
-	// const initPromiseFlag = <const>"initPromiseFlag";
-
 	promises.reduce<(...args: any) => Promise<T>>(
 		function (previousPromise, nextPromise) {
 			return function () {
-				return previousPromise().then(() => nextPromise());
+				return previousPromise().then((res) => nextPromise(res));
 			};
 		},
 		function () {
