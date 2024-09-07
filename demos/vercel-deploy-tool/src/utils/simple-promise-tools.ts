90bc3213e262a4a0a144c4e9771e6936dd922fff
ruan-cat <1219043956@qq.com>
2024-09-07 22:24:54 +0800

    refactor: 优化runPromiseByQueue函数
    优化runPromiseByQueue函数，将其从内部函数改为导出的函数，以便在其他模块中使用。
    
    修改了demos/vercel-deploy-tool/src/utils/simple-promise-tools.ts文件。
    
    修改了tsconfig.json文件，将rootDir设置为"."，并添加了"tests"目录到rootDirs中。
    
    添加了runPromiseByQueue.test.ts文件和vitest.config.ts文件。

--- demos/vercel-deploy-tool/src/utils/simple-promise-tools.ts
+++ demos/vercel-deploy-tool/src/utils/simple-promise-tools.ts
@@ -2,7 +2,7 @@
  * 以队列串行的形式 运行异步函数
  * @see https://github.com/ascoders/weekly/blob/master/前沿技术/77.精读《用%20Reduce%20实现%20Promise%20串行执行》.md
  */
-function runPromiseByQueue<T>(promises: ((...args: any) => Promise<T>)[]) {
+export function runPromiseByQueue<T>(promises: ((...args: any) => Promise<T>)[]) {
 	promises.reduce<(...args: any) => Promise<T>>(
 		function (previousPromise, nextPromise) {
 			return function () {
---
+++ demos/vercel-deploy-tool/tests/runPromiseByQueue.test.ts
@@ -0,0 +1,6 @@
+import { expect, test } from "vitest";
+import { runPromiseByQueue } from "./utils/simple-promise-tools";
+
+// test("adds 1 + 2 to equal 3", () => {
+// 	expect(sum(1, 2)).toBe(3);
+// });
