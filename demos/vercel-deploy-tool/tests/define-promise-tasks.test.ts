f36c6298ea73fdfd76193469549d53c42b6e87e3
ruan-cat <1219043956@qq.com>
2024-09-08 14:49:14 +0800

    🧪 test: 测试 executePromiseTasks ；

--- demos/vercel-deploy-tool/tests/define-promise-tasks.test.ts
+++ demos/vercel-deploy-tool/tests/define-promise-tasks.test.ts
@@ -1,7 +1,7 @@
 import { consola } from "consola";
 import { uniqueId } from "lodash-es";
 
-import { definePromiseTasks } from "../src/utils/define-promise-tasks";
+import { definePromiseTasks, executePromiseTasks } from "../src/utils/define-promise-tasks";
 import { generateSimpleAsyncTask, wait } from "../src/utils/simple-promise-tools";
 
 function generateArray<T>(params: { length: number; content: T }) {
@@ -12,14 +12,16 @@ function generateArray<T>(params: { length: number; content: T }) {
 		});
 }
 
-const counter = uniqueId("");
+const getCounter = () => uniqueId();
 
 async function link() {
-	consola.start(" 开始链接任务 ");
-	consola.info(` 这是 ${counter + 1} 个任务`);
-	await wait(300);
-	consola.success(" 完成链接任务 ");
-	console.log("\n");
+	const taskId = getCounter();
+
+	consola.start(` 开始 ${taskId} 链接任务 `);
+	await wait(250);
+	consola.success(` 完成 ${taskId} 链接任务 `);
+
+	// console.log("\n");
 }
 
 const promiseTasksConfig = definePromiseTasks({
@@ -62,3 +64,5 @@ const promiseTasksConfig = definePromiseTasks({
 		},
 	],
 });
+
+executePromiseTasks(promiseTasksConfig);
