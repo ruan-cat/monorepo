4e33b5c4e4abd34a770698233887a6e1aa9af0b6
ruan-cat <1219043956@qq.com>
2024-09-08 15:26:57 +0800

    🧪 test: 编写模拟的异步任务；

--- demos/vercel-deploy-tool/tests/define-promise-tasks.test.ts
+++ demos/vercel-deploy-tool/tests/define-promise-tasks.test.ts
@@ -14,17 +14,49 @@ function generateArray<T>(params: { length: number; content: T }) {
 
 const getCounter = () => uniqueId();
 
-async function link() {
+async function preEciProjDir() {
 	const taskId = getCounter();
-
-	consola.start(` 开始 ${taskId} 链接任务 `);
-	await wait(250);
-	consola.success(` 完成 ${taskId} 链接任务 `);
-
-	// console.log("\n");
+	consola.start(` 开始 ${taskId} 预构建eci任务 `);
+	await wait(10);
+	consola.success(` 完成 ${taskId} 预构建eci任务 `);
 }
 
-const promiseTasksConfig = definePromiseTasks({
+async function link() {
+	const taskId = getCounter();
+	consola.start(` 开始 ${taskId} 链接任务 `);
+	await wait(10);
+	consola.success(` 完成 ${taskId} 链接任务 `);
+}
+
+async function build() {
+	const taskId = getCounter();
+	consola.start(` 开始 ${taskId} 构建任务 `);
+	await wait(10);
+	consola.success(` 完成 ${taskId} 构建任务 `);
+}
+
+async function buildAll() {
+	const taskId = getCounter();
+	consola.start(` 开始 ${taskId} turbo集体构建任务 `);
+	await wait(10);
+	consola.success(` 完成 ${taskId} turbo集体构建任务 `);
+}
+
+async function copyDist() {
+	const taskId = getCounter();
+	consola.start(` 开始 ${taskId} 移动目录任务 `);
+	await wait(10);
+	consola.success(` 完成 ${taskId} 移动目录任务 `);
+}
+
+async function deploy() {
+	const taskId = getCounter();
+	consola.start(` 开始 ${taskId} 部署任务 `);
+	await wait(10);
+	consola.success(` 完成 ${taskId} 部署任务 `);
+}
+
+const promiseTasksConfig1 = definePromiseTasks({
 	type: "queue",
 	tasks: [
 		{
@@ -65,4 +97,61 @@ const promiseTasksConfig = definePromiseTasks({
 	],
 });
 
-executePromiseTasks(promiseTasksConfig);
+const promiseTasksConfig2 = definePromiseTasks({
+	type: "single",
+	tasks: {
+		type: "queue",
+
+		tasks: [
+			// 某个预构建任务
+			{
+				type: "single",
+				tasks: generateSimpleAsyncTask(preEciProjDir),
+			},
+
+			// 并发的链接任务
+			{
+				type: "parallel",
+				tasks: generateArray({
+					length: 3,
+					content: generateSimpleAsyncTask(link),
+				}),
+			},
+
+			// 并发的构建任务
+			{
+				type: "parallel",
+				tasks: generateArray({
+					length: 3,
+					content: generateSimpleAsyncTask(build),
+				}),
+			},
+
+			// 某个集体构建任务
+			{
+				type: "single",
+				tasks: generateSimpleAsyncTask(buildAll),
+			},
+
+			// 移动目录任务
+			{
+				type: "parallel",
+				tasks: generateArray({
+					length: 3,
+					content: generateSimpleAsyncTask(copyDist),
+				}),
+			},
+
+			// 部署任务
+			{
+				type: "parallel",
+				tasks: generateArray({
+					length: 3,
+					content: generateSimpleAsyncTask(deploy),
+				}),
+			},
+		],
+	},
+});
+
+executePromiseTasks(promiseTasksConfig2);
