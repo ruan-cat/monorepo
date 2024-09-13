db13ecfa89c3603da58301e6a2cf634ce49b977f
ruan-cat <1219043956@qq.com>
2024-09-13 22:03:36 +0800

    ✨ feat: 提供代码处理策略；

--- utils/tests/rmmv-class-expand-tools.ts
+++ utils/tests/rmmv-class-expand-tools.ts
@@ -78,6 +78,20 @@ const handleStrategy = <const>[
  */
 type HandleStrategy = (typeof handleStrategy)[number];
 
+/**
+ * 被默认执行默认处理策略的函数名
+ * @description
+ * 初始化函数默认使用固定的处理策略。
+ */
+type defaultHandleStrategy_FuncationName = FunctionKeys<RmmvClass>;
+
+/** 没有初始化函数的全部函数名 */
+type FunctionKeysWithoutInitialize<T> = Exclude<FunctionKeys<T>, defaultHandleStrategy_FuncationName>;
+
+/** 全部有意义函数的 处理策略配置 */
+type HandleStrategyConfig<T> = Record<FunctionKeysWithoutInitialize<T>, HandleStrategy>;
+
+/** rmmv类拓展工具函数配置 */
 type RmmvClassExpandTools<SourceCode extends RmmvClass, UserCode> = {
 	/** 源码 一般是被拓展的类，往往是rmmv的源码类 */
 	source: SourceCode;
@@ -90,12 +104,41 @@ type RmmvClassExpandTools<SourceCode extends RmmvClass, UserCode> = {
 	 * @description
 	 * 用户可以不提供配置 就默认按照标准的方式处理
 	 */
-	config?: Partial<Record<FunctionKeys<UserCode>, HandleStrategy>>;
+	config?: Partial<HandleStrategyConfig<UserCode>>;
 };
 
+/** rmmv类拓展工具函数 */
 function rmmvClassExpandTools<SourceCode extends RmmvClass, UserCode = any>(
 	params: RmmvClassExpandTools<SourceCode, UserCode>,
-) {}
+) {
+	// const { source, userCode, config } = params;
+	// const handleStrategyConfig = {
+	// 	...config,
+	// };
+	// const userCodeKeys = Object.keys(userCode) as FunctionKeysWithoutInitialize<UserCode>[];
+	// for (const key of userCodeKeys) {
+	// 	const handleData = handleStrategyConfig[key] ?? defaultHandleStrategy;
+	// 	const sourceFunction = source[key];
+	// 	const userCodeFunction = userCode[key];
+	// 	switch (handleData) {
+	// 		case "userCode-cover-source":
+	// 			source[key] = userCodeFunction;
+	// 			break;
+	// 		case "userCode-first":
+	// 			source[key] = function () {
+	// 				userCodeFunction.apply(this, arguments);
+	// 				sourceFunction.apply(this, arguments);
+	// 			};
+	// 			break;
+	// 		default:
+	// 			source[key] = function () {
+	// 				sourceFunction.apply(this, arguments);
+	// 				userCodeFunction.apply(this, arguments);
+	// 			};
+	// 			break;
+	// 	}
+	// }
+}
 
 rmmvClassExpandTools({
 	source: SimpleBaseClass as unknown as SimpleBaseClass,
@@ -104,37 +147,3 @@ rmmvClassExpandTools({
 		handleData: "userCode-first",
 	},
 });
-
-// satisfies RmmvClass
-
-//  DrillUpClass
-
-// function DrillUpClass() {
-// 	this.initialize.apply(this, arguments);
-// }
-
-// Stage
-// function Scene_Base() {
-// 	//调用 初始化
-// 	this.initialize.apply(this, arguments);
-// }
-
-// //设置原形
-// Scene_Base.prototype = Object.create(Stage.prototype);
-
-// //设置创造者
-// Scene_Base.prototype.constructor = Scene_Base;
-
-// //初始化
-// Scene_Base.prototype.initialize = function () {
-// 	//舞台 初始化 呼叫(this)
-// 	Stage.prototype.initialize.call(this);
-// 	//活动标志 关闭
-// 	this._active = false;
-// 	//设置 淡入记号
-// 	this._fadeSign = 0;
-// 	//设置 淡入持续时间
-// 	this._fadeDuration = 0;
-// 	//设置 淡入精灵
-// 	this._fadeSprite = null;
-// };
