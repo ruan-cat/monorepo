6793af225ca371dbc04ea594a14710328e65fd82
ruan-cat <1219043956@qq.com>
2024-09-18 22:38:33 +0800

    🧪 test: 提供测试配置；

--- .config/vercel-deploy-tool.ts
+++ .config/vercel-deploy-tool.ts
@@ -8,6 +8,7 @@ export default <Config>{
 	afterBuildTasks: ["pnpm run turbo build:docs"],
 
 	deployTargets: [
+		// 01星球建议笔记
 		{
 			type: "userCommands",
 			targetCWD: "./packages/docs-01-star",
@@ -16,20 +17,58 @@ export default <Config>{
 			userCommands: ["pnpm -C=./packages/docs-01-star build:docs"],
 		},
 
-		// {
-		// 	type: "userCommands",
-		// 	targetCWD: "./packages/monorepo-5",
-		// 	outputDirectory: "src/.vuepress/dist/**/*",
-		// 	url: ["monorepo-5.ruancat6312.top", "monorepo5.ruan-cat.com"],
-		// 	userCommands: ["pnpm -C=./packages/monorepo-5 build:docs"],
-		// },
+		// 1号项目
+		{
+			type: "userCommands",
+			targetCWD: "./packages/monorepo-1",
+			outputDirectory: "src/.vuepress/dist/**/*",
+			url: ["monorepo-1.ruancat6312.top"],
+			userCommands: ["pnpm -C=./packages/monorepo-1 build:docs"],
+		},
 
-		// {
-		// 	type: "static",
-		// 	targetCWD: "./demos/gh.HFIProgramming.mikutap",
-		// 	url: ["mikutap.ruancat6312.top"],
-		// 	// 测试类型约束是否到位。
-		// 	// userCommands: ["echo 'mikutap1'", "echo 'mikutap2'", "echo 'mikutap3'"],
-		// },
+		// 2号项目
+		{
+			type: "userCommands",
+			targetCWD: "./packages/proj-2-vp2-custom-components",
+			outputDirectory: "src/.vuepress/dist/**/*",
+			url: ["monorepo-2.ruancat6312.top", "m2.ruan-cat.com", "m2.ruancat6312.top"],
+			userCommands: ["pnpm -C=./packages/proj-2-vp2-custom-components build:docs"],
+		},
+
+		// 3号项目
+		{
+			type: "userCommands",
+			targetCWD: "./packages/monorepo-3",
+			outputDirectory: "src/.vuepress/dist/**/*",
+			url: ["monorepo-3.ruancat6312.top"],
+			userCommands: ["pnpm -C=./packages/monorepo-3 build:docs"],
+		},
+
+		// 4号项目
+		{
+			type: "userCommands",
+			targetCWD: "./packages/monorepo-4",
+			outputDirectory: "src/.vuepress/dist/**/*",
+			url: ["monorepo-4.ruancat6312.top"],
+			userCommands: ["pnpm -C=./packages/monorepo-4 build:docs"],
+		},
+
+		// 5号项目
+		{
+			type: "userCommands",
+			targetCWD: "./packages/monorepo-5",
+			outputDirectory: "src/.vuepress/dist/**/*",
+			url: ["monorepo-5.ruancat6312.top", "monorepo5.ruan-cat.com"],
+			userCommands: ["pnpm -C=./packages/monorepo-5 build:docs"],
+		},
+
+		// mikutap项目
+		{
+			type: "static",
+			targetCWD: "./demos/gh.HFIProgramming.mikutap",
+			url: ["mikutap.ruancat6312.top"],
+			// 测试类型约束是否到位。
+			// userCommands: ["echo 'mikutap1'", "echo 'mikutap2'", "echo 'mikutap3'"],
+		},
 	],
 };
