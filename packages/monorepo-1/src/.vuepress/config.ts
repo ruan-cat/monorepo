ad0cb281ade5e114575433f785adc54214ac9456
ruan-cat <1219043956@qq.com>
2024-09-21 19:10:27 +0800

    ✨ feat: 改造了 monorepo-1 项目的文档生成配置；

--- packages/monorepo-1/src/.vuepress/config.ts
+++ packages/monorepo-1/src/.vuepress/config.ts
@@ -1,17 +1,5 @@
-// import "@ruan-cat/vuepress-preset-config/dist/style.css";
-import { defineRuanCatVuepressPresetConfig, defineRuanCatVuepressConfig } from "@ruan-cat/vuepress-preset-config";
+import { defineRuanCatVuepressConfig } from "@ruan-cat/vuepress-preset-config";
 
-import { getDirname, path } from "vuepress/utils";
-
-const __dirname = getDirname(import.meta.url);
-
-// export default defineRuanCatVuepressPresetConfig;
 export default defineRuanCatVuepressConfig({
-	alias: {
-		// "@theme-hope/modules/navbar/components/RepoLink": new URL("./components/test.vue", import.meta.url),
-		// fileURLToPath(
-		// 	new URL("./components/test.vue", import.meta.url),
-		// ),
-		"@theme-hope/modules/navbar/components/RepoLink": path.resolve(__dirname, "./components/test.vue"),
-	},
+	title: "monorepo-1",
 });
