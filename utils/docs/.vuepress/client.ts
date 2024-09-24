4cf5c0e484470c85216bbf43d84e5c34d0f3d71c
ruan-cat <1219043956@qq.com>
2024-09-24 11:27:04 +0800

    ✨ feat: 在rc56内，完成了样式设置；

--- utils/docs/.vuepress/client.ts
+++ utils/docs/.vuepress/client.ts
@@ -1,3 +1,4 @@
 // import "@ruan-cat/vuepress-preset-config/src/styles/common/index.scss";
 // import "@ruan-cat/vuepress-preset-config/src/styles/drill-style/hope-use-palette.scss";
+// import "@ruan-cat/vuepress-preset-config/src/styles/drill-style/hope-use-config.scss";
 import "@ruan-cat/vuepress-preset-config/src/styles/drill-style/index.scss";
