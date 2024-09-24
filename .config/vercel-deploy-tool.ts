040b0a3fc5062579766927351989bfec01a8cdba
ruan-cat <1219043956@qq.com>
2024-09-24 16:49:02 +0800

    🐎 ci: 配置工具包项目的自动部署行为；

--- .config/vercel-deploy-tool.ts
+++ .config/vercel-deploy-tool.ts
@@ -21,7 +21,6 @@ export default <Config>{
 		// 		// "pnpm turbo build:docs",
 		// 	],
 		// },
-
 		// 1号项目
 		// {
 		// 	type: "userCommands",
@@ -34,20 +33,18 @@ export default <Config>{
 		// 		// "pnpm turbo build:docs",
 		// 	],
 		// },
-
-		// 2号项目
-		{
-			type: "userCommands",
-			targetCWD: "./packages/proj-2-vp2-custom-components",
-			outputDirectory: "src/.vuepress/dist/**/*",
-			url: ["monorepo-2.ruancat6312.top", "m2.ruan-cat.com", "m2.ruancat6312.top"],
-			userCommands: [
-				// "pnpm -C=./packages/proj-2-vp2-custom-components build:docs"
-				// "pnpm -C='./' turbo build:docs",
-				// "pnpm turbo build:docs",
-			],
-		},
-
+		// 2号项目 通过测试 多域名可以实现部署
+		// {
+		// 	type: "userCommands",
+		// 	targetCWD: "./packages/proj-2-vp2-custom-components",
+		// 	outputDirectory: "src/.vuepress/dist/**/*",
+		// 	url: ["monorepo-2.ruancat6312.top", "m2.ruan-cat.com", "m2.ruancat6312.top"],
+		// 	userCommands: [
+		// 		// "pnpm -C=./packages/proj-2-vp2-custom-components build:docs"
+		// 		// "pnpm -C='./' turbo build:docs",
+		// 		// "pnpm turbo build:docs",
+		// 	],
+		// },
 		// 3号项目 通过测试
 		// {
 		// 	type: "userCommands",
@@ -56,7 +53,6 @@ export default <Config>{
 		// 	url: ["monorepo-3.ruancat6312.top"],
 		// 	userCommands: ["pnpm -C=./packages/monorepo-3 build:docs"],
 		// },
-
 		// 4号项目 通过测试
 		// {
 		// 	type: "userCommands",
@@ -65,7 +61,6 @@ export default <Config>{
 		// 	url: ["monorepo-4.ruancat6312.top"],
 		// 	userCommands: ["pnpm -C=./packages/monorepo-4 build:docs"],
 		// },
-
 		// 5号项目 通过测试
 		// {
 		// 	type: "userCommands",
@@ -74,14 +69,23 @@ export default <Config>{
 		// 	url: ["monorepo-5.ruancat6312.top", "monorepo5.ruan-cat.com"],
 		// 	userCommands: ["pnpm -C=./packages/monorepo-5 build:docs"],
 		// },
+		// mikutap项目 测试通过
+		// mikutap项目是一个原生H5项目 是一堆纯静态文件 所以没必要频繁部署
+		// {
+		// 	type: "static",
+		// 	targetCWD: "./demos/gh.HFIProgramming.mikutap",
+		// 	url: ["mikutap.ruancat6312.top"],
+		// 	// 测试类型约束是否到位。
+		// 	// userCommands: ["echo 'mikutap1'", "echo 'mikutap2'", "echo 'mikutap3'"],
+		// },
 
-		// mikutap项目
+		// 工具包文档项目
 		{
-			type: "static",
-			targetCWD: "./demos/gh.HFIProgramming.mikutap",
-			url: ["mikutap.ruancat6312.top"],
-			// 测试类型约束是否到位。
-			// userCommands: ["echo 'mikutap1'", "echo 'mikutap2'", "echo 'mikutap3'"],
+			type: "userCommands",
+			targetCWD: "./utils",
+			outputDirectory: "docs/.vuepress/dist/**/*",
+			url: ["utils.ruancat6312.top"],
+			userCommands: [],
 		},
 	],
 };
