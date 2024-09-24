c47fad54935badce6790ef2b59a60e2be1f4f8ec
ruan-cat <1219043956@qq.com>
2024-09-24 16:35:00 +0800

    🤔 save-file: 编写注释，移除不必要的文件；

--- utils/docs/.vuepress/client.ts
+++ utils/docs/.vuepress/client.ts
@@ -1,4 +1,4 @@
+// 测试 普通样式
 // import "@ruan-cat/vuepress-preset-config/src/styles/common/index.scss";
-// import "@ruan-cat/vuepress-preset-config/src/styles/drill-style/hope-use-palette.scss";
-// import "@ruan-cat/vuepress-preset-config/src/styles/drill-style/hope-use-config.scss";
+// 测试 爱丽丝样式
 import "@ruan-cat/vuepress-preset-config/src/styles/drill-style/index.scss";
