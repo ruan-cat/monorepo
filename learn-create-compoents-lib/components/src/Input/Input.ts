4efc74c58b5eda69b51fae18647c116e90c6b769
ruan-cat <1219043956@qq.com>
2024-09-29 11:15:25 +0800

    🎉 init: 正在逐步抄组件封装配置，学习封装架构，和边缘情况的处理方式；

---
+++ learn-create-compoents-lib/components/src/Input/Input.ts
@@ -0,0 +1,32 @@
+/**
+ * Input.ts —
+ * 用于定义类型文件，如Input的props类型，emit类型和instance类型等，
+ *
+ * 内容如下：
+ *
+ * InputInstance是用来干啥的？
+ * 在写公共组件时，我们会使用defineExpose暴露一些方法。
+ * 如在element-plus中，就会使用formRef.validate 来校验表单，
+ * instance里就有暴露方法的类型签名。
+ */
+import Input from "./Input.vue";
+
+/**
+ * 定义props类型
+ */
+export interface InputProps {
+	modelValue: string;
+	disabled?: boolean;
+}
+
+/**
+ * 定义emit类型
+ */
+export type InputEmits = {
+	"update:modelValue": [value: string];
+};
+
+/**
+ * 定义instance类型
+ */
+export type InputInstance = InstanceType<typeof Input>;
