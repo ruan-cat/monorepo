e9a78e2e1bed30b6456967e68ffcde71541c4f55
ruan-cat <1219043956@qq.com>
2024-09-30 15:11:10 +0800

    ✨ feat: 编写测试代码；

--- learn-create-compoents-lib/test-app/src/main.ts
+++ learn-create-compoents-lib/test-app/src/main.ts
@@ -1,5 +1,12 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
 
-createApp(App).mount('#app')
+import "./style.css";
+import App from "./App.vue";
+
+import "@giegie/components/es/style.css";
+import GieComponents from "@giegie/components";
+
+createApp(App)
+	//
+	.use(GieComponents)
+	.mount("#app");
