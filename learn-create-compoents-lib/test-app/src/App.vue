e746ad5c3ddbb94a1d3ae78f0ea36640e30897c8
ruan-cat <1219043956@qq.com>
2024-09-30 16:27:13 +0800

    🤔 save-file: 尝试实现类型声明；

--- learn-create-compoents-lib/test-app/src/App.vue
+++ learn-create-compoents-lib/test-app/src/App.vue
@@ -1,7 +1,8 @@
 <template>
 	<div>
 		<!-- FIXME: 实现volar提供的组件类型声明 -->
-		<gie-input v-model="state" ref="inputRef" />
+		<!-- <gie-input v-model="state" ref="inputRef" /> -->
+		<GieInput v-model="state" ref="inputRef" />
 		{{ state }}
 		<button @click="onFocus">focus</button>
 	</div>
--- learn-create-compoents-lib/test-app/vite.config.ts
+++ learn-create-compoents-lib/test-app/vite.config.ts
@@ -4,9 +4,21 @@ import Components from "unplugin-vue-components/vite";
 import AutoImport from "unplugin-auto-import/vite";
 
 // FIXME: 处理类型声明问题 这是一个纯js文件
-import { GieResolver } from "@giegie/resolver";
+// import { GieResolver } from "@giegie/resolver";
 
 // console.log(" GieResolver ", GieResolver);
+
+function GieResolver_2(name: string) {
+	console.log(" in name ", name);
+	if (name.startsWith("Gie")) {
+		const partialName = name.slice(3);
+		return {
+			name: "Gie" + partialName,
+			from: `@giegie/components`,
+			sideEffects: `@giegie/components/es/${partialName}/style/index.css`,
+		};
+	}
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -17,12 +29,24 @@ export default defineConfig({
 		vue(),
 
 		Components({
-			dts: true,
-			resolvers: [GieResolver()],
+			// dts: true,
+			resolvers: [
+				// GieResolver()
+				{
+					type: "component",
+					resolve: GieResolver_2,
+				},
+			],
 		}),
 
 		AutoImport({
-			resolvers: [GieResolver()],
+			resolvers: [
+				// GieResolver()
+				{
+					type: "component",
+					resolve: GieResolver_2,
+				},
+			],
 		}),
 	],
 });
