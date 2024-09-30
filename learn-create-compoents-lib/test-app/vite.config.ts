87ce0c01db4dd88dd95cabdfbcd3bd276b98d2f4
ruan-cat <1219043956@qq.com>
2024-09-30 15:57:42 +0800

    ✨ feat: 导入组件解析器；

--- learn-create-compoents-lib/test-app/vite.config.ts
+++ learn-create-compoents-lib/test-app/vite.config.ts
@@ -1,10 +1,25 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import Components from "unplugin-vue-components/vite";
+import AutoImport from "unplugin-auto-import/vite";
+
+// FIXME: 处理类型声明问题 这是一个纯js文件
+import { GieResolver } from "@giegie/resolver";
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	server: {
 		port: 8080,
 	},
-	plugins: [vue()],
+	plugins: [
+		vue(),
+
+		Components({
+			resolvers: [GieResolver()],
+		}),
+
+		AutoImport({
+			resolvers: [GieResolver()],
+		}),
+	],
 });
