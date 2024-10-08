62b1a6464c2289f5b8b3ecb3d9c3d82bb90230a2
ruan-cat <1219043956@qq.com>
2024-10-08 11:08:08 +0800

    ⚙️ config: 使用新的工具，生成新的依赖；

--- learn-create-compoents-lib/components/vite.config.ts
+++ learn-create-compoents-lib/components/vite.config.ts
@@ -1,12 +1,22 @@
 // 在 components 下 新建一个vite.config.ts文件，配置和说明如下：
 import { uniqueId } from "lodash-es";
 
+import { resolve } from "node:path";
+import fs from "node:fs";
+
 import { defineConfig } from "vite";
 import type { UserConfig } from "vite";
 import Components from "unplugin-vue-components/vite";
 import vue from "@vitejs/plugin-vue";
 import dts from "vite-plugin-dts";
 import { GieResolver } from "@giegie/resolver";
+import { createPlugin } from "vite-plugin-autogeneration-import-file";
+
+const { autoImport, resolver } = createPlugin();
+function pathResolve(dir: string) {
+	//路径转化
+	return resolve(__dirname, ".", dir);
+}
 
 export default defineConfig(() => {
 	return {
@@ -57,6 +67,39 @@ export default defineConfig(() => {
 
 		plugins: [
 			vue(),
+
+			autoImport([
+				// 自动生成
+				{
+					// auto import components
+					pattern: ["*.{vue,ts}", "**/index.{vue,ts}"], //监听的文件规则当前规则为 监听文件夹下的后缀为vue/ts子文件和 文件名为index.vue和index.ts的子孙文件
+					dir: pathResolve("src/components"), //监听的文件夹
+					toFile: pathResolve("types/components.d.ts"), //生成的文件
+					template: fs.readFileSync(pathResolve("./template/components.d.ts"), "utf-8"), //文件生成模板
+					codeTemplates: [
+						//代码模板
+						{
+							key: "//code",
+							template: '{{name}}: typeof import("{{path}}")["default"];\n    ',
+						},
+						{
+							key: "//typeCode",
+							template: 'type {{name}}Instance = InstanceType<typeof import("{{path}}")["default"]>;\n  ',
+						},
+					],
+					name: "_{{name}}", //组件名命名规则支持字符串模板和函数
+				},
+			]),
+
+			Components({
+				// 忽略掉'unplugin-vue-components'的组件引入
+				dirs: [],
+				// ui库解析器
+				resolvers: [resolver([0])],//应用vite-plugin-autogeneration-import-file插件的第0组规则进行组件引入
+				//禁止生成component.d.ts
+				dts: false
+			})
+	
 
 			// 尝试在这里自己生成类型声明文件，生成 GlobalComponents 接口的内容。
 			// 失败 unplugin-vue-components/vite 提供的自动生成类型，不能深度地配置
