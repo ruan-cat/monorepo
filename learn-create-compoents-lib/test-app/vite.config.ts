222c5a65baf0555b7d972ec6516246d7dc255e11
ruan-cat <1219043956@qq.com>
2024-10-09 17:39:05 +0800

    🤔 save-file: 尝试测试输出内容；

--- learn-create-compoents-lib/test-app/vite.config.ts
+++ learn-create-compoents-lib/test-app/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import Components from "unplugin-vue-components/vite";
 import AutoImport from "unplugin-auto-import/vite";
+import { visualizer } from "rollup-plugin-visualizer";
 
 // FIXME: 处理类型声明问题 这是一个纯js文件
 // import { GieResolver } from "@giegie/resolver";
@@ -25,8 +26,21 @@ export default defineConfig({
 	server: {
 		port: 8080,
 	},
+
+	// build: {},
+
 	plugins: [
 		vue(),
+
+		visualizer({
+			// filename: "dist/visualizer/index.html",
+			// TODO: 未来这里需要考虑实现vuepress的路由配置，在vuepress内加上一个路由来导航。
+			// 我们本地生成了一大堆html静态页面，如何对接接洽vuepress的路由？
+			// typedoc好像有类似的实现方案。
+			filename: "dist/visualizer/index.html",
+			title: "visualizer打包分析报告",
+			template: "network",
+		}),
 
 		Components({
 			// dts: true,
