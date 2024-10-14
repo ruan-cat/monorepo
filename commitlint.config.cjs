7bd0081d126befb9c90f32abc0b54cf66d97a4f4
ruan-cat <1219043956@qq.com>
2024-10-14 15:39:38 +0800

    🤔 save-file: 疑似可以实现更改；暂时保存；

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -14,6 +14,8 @@ const fs = require("fs");
 const path = require("path");
 const glob = require("glob");
 const yaml = require("js-yaml");
+// const { createRequire } = require("module");
+// const esm = require("esm")(module);
 
 const defScopes = [
 	"root|根目录",
@@ -24,6 +26,15 @@ const defScopes = [
 ];
 
 /**
+ * 路径转换工具
+ * @param { string } path 路径
+ * @returns { string } 改造后的路径
+ */
+function pathChange(path) {
+	return path.replace(/\\/g, "/");
+}
+
+/**
  * 根据 pnpm-workspace.yaml 配置的monorepo有意义的包名，获取包名和包描述
  * @description
  * 根据 pnpm-workspace.yaml 配置的monorepo有意义的包名，获取包名和包描述
@@ -32,9 +43,13 @@ const defScopes = [
  * @return { Promise<import("cz-git").ScopesType> }
  */
 async function getPackagesNameAndDescription() {
+	//  const require = createRequire(import.meta.url);
+	// /**
+	//  * @see https://juejin.cn/post/7240365776737730616
+	//  */
+	//   const lodash = require("lodash-es");
+	// const lodash = esm("lodash-es");
 	const lodash = await import("lodash-es");
-
-	console.log("lodash :>> ", lodash);
 
 	// 读取 pnpm-workspace.yaml 文件 文件路径
 	const workspaceConfigPath = path.join(__dirname, "pnpm-workspace.yaml");
@@ -46,9 +61,8 @@ async function getPackagesNameAndDescription() {
 	 * pnpm-workspace.yaml 的配置
 	 * @type { import("./types/pnpm-workspace.yaml.shim.ts").PnpmWorkspace }
 	 */
+	// @ts-ignore 忽略unknown的警告
 	const workspaceConfig = yaml.load(workspaceFile);
-
-	console.log("workspaceConfig :>> ", workspaceConfig);
 
 	/**
 	 * packages配置 包的匹配语法
@@ -60,39 +74,26 @@ async function getPackagesNameAndDescription() {
 		return defScopes;
 	}
 
-	console.log("pkgPatterns :>> ", pkgPatterns);
-
-	// // 存储匹配的 package.json 文件路径
-	// let pkgPaths = [];
-	// // 根据每个模式匹配相应的目录
-	// pkgPatterns.forEach((pattern) => {
-	// 	const matchedPath = path.join(__dirname, pattern, "package.json").replace(/\\/g, "/");
-	// 	console.log("matchedPath :>> ", matchedPath);
-	// 	const matchedPaths = glob.sync(matchedPath, {
-	// 		ignore: "**/node_modules/**",
-	// 	});
-	// 	pkgPaths = pkgPaths.concat(matchedPaths);
-	// });
-	// // 输出匹配的 package.json 文件路径
-	// console.log(pkgPaths);
-
 	/**
 	 * 全部的 package.json 文件路径
 	 * @type { string[] }
 	 */
-	const pkgPaths = [];
+	let pkgPaths = [];
 
 	// 根据每个模式匹配相应的目录
-	pkgPatterns.forEach((pkgPattern) => {
-		const matchedPath = path.join(__dirname, pkgPattern, "package.json").replace(/\\/g, "/");
-		console.log("matchedPath :>> ", matchedPath);
+	pkgPatterns.map((pkgPattern) => {
+		const matchedPath = pathChange(path.join(__dirname, pkgPattern, "package.json"));
+
 		const matchedPaths = glob.sync(matchedPath, {
 			ignore: "**/node_modules/**",
 		});
 
 		// 找到包路径，就按照顺序逐个填充准备
-		pkgPaths.concat(matchedPaths);
+		pkgPaths = pkgPaths.concat(...matchedPaths);
+		return matchedPaths;
 	});
+
+	// console.log("pkgPaths :>> ", pkgPaths);
 
 	/**
 	 * @returns { import("cz-git").ScopesType }
@@ -120,108 +121,108 @@ async function getPackagesNameAndDescription() {
 		};
 	});
 
+	// console.log("czGitScopesType :>> ", czGitScopesType);
+
 	return czGitScopesType;
 }
 
-getPackagesNameAndDescription();
+let scopes = defScopes;
 
-// .commitlintrc.js
-/** @type {import("cz-git").UserConfig} */
-module.exports = {
-	rules: {
-		// @see: https://commitlint.js.org/#/reference-rules
-	},
-	prompt: {
-		alias: { fd: "docs: fix typos" },
-
-		messages: {
-			type: "选择你要提交的类型 :",
-			scope: "选择一个提交范围（可选）:",
-			customScope: "请输入自定义的提交范围 :",
-			subject: "填写简短精炼的变更描述 :\n",
-			body: '填写更加详细的变更描述（可选）。使用 "|" 换行 :\n',
-			breaking: '列举非兼容性重大的变更（可选）。使用 "|" 换行 :\n',
-			footerPrefixesSelect: "选择关联issue前缀（可选）:",
-			customFooterPrefix: "输入自定义issue前缀 :",
-			footer: "列举关联issue (可选) 例如: #31, #I3244 :\n",
-			confirmCommit: "是否提交或修改commit ?",
+(async () => {
+	scopes = await getPackagesNameAndDescription();
+	console.log(" in  scopes ", scopes);
+	// .commitlintrc.js
+	/** @type {import("cz-git").UserConfig} */
+	module.exports = {
+		rules: {
+			// @see: https://commitlint.js.org/#/reference-rules
 		},
+		prompt: {
+			alias: { fd: "docs: fix typos" },
 
-		/**
-		 * 基于monorepo内项目，决定提交范围域
-		 */
-		scopes: [
-			"root|根目录",
-			"vuepress-preset-config|vp2预设配置",
-			"vercel-deploy-tool|vc部署工具",
-			"utils|工具包",
-			"demo|测试项目",
-		],
-
-		// https://cz-git.qbb.sh/zh/recipes/#多选模式
-		enableMultipleScopes: true,
-		scopeEnumSeparator: ",",
-
-		allowCustomScopes: true,
-		allowEmptyScopes: true,
-		customScopesAlign: "bottom",
-		customScopesAlias: "custom",
-		emptyScopesAlias: "empty",
-
-		types: [
-			{ value: "✨ feat", name: "✨ feat:     新增功能 | A new feature" },
-			{ value: "🐞 fix", name: "🐞 fix:      修复缺陷 | A bug fix" },
-			{ value: "📃 docs", name: "📃 docs:     文档更新 | Documentation only changes" },
-			{ value: "🌈 style", name: "🌈 style:    代码格式 | Changes that do not affect the meaning of the code" },
-			{
-				value: "🦄 refactor",
-				name: "🦄 refactor: 代码重构 | A code change that neither fixes a bug nor adds a feature",
+			messages: {
+				type: "选择你要提交的类型 :",
+				scope: "选择一个提交范围（可选）:",
+				customScope: "请输入自定义的提交范围 :",
+				subject: "填写简短精炼的变更描述 :\n",
+				body: '填写更加详细的变更描述（可选）。使用 "|" 换行 :\n',
+				breaking: '列举非兼容性重大的变更（可选）。使用 "|" 换行 :\n',
+				footerPrefixesSelect: "选择关联issue前缀（可选）:",
+				customFooterPrefix: "输入自定义issue前缀 :",
+				footer: "列举关联issue (可选) 例如: #31, #I3244 :\n",
+				confirmCommit: "是否提交或修改commit ?",
 			},
-			{ value: "🎈 perf", name: "🎈 perf:     性能提升 | A code change that improves performance" },
-			{ value: "🧪 test", name: "🧪 test:     测试相关 | Adding missing tests or correcting existing tests" },
-			{
-				value: "🔧 build",
-				name: "🔧 build:    构建相关 | Changes that affect the build system or external dependencies",
-			},
-			{ value: "🐎 ci", name: "🐎 ci:       持续集成 | Changes to our CI configuration files and scripts" },
-			{ value: "↩ revert", name: "↩ revert:   回退代码 | Revert to a commit" },
-			{ value: "🐳 chore", name: "🐳 chore:    其他修改 | Other changes that do not modify src or test files" },
-			{
-				value: "🤔 save-file",
-				name: "🤔 save-file:    保存文件 | 文件保存类型。仅仅是为了保存文件。有时候会需要紧急提交，并快速切换分支。此时就需要提交代码。并保存文件。",
-			},
-			{ value: "⚙️ config", name: "⚙️ config:    更新配置 | 配置更新。通用性的配置更新。" },
-			{ value: "✋ main-pull-update", name: "✋ main-pull-update:    主分支拉取更新 | 主分支拉取更新。" },
-			{ value: "🗑 del", name: "🗑 del:    删除垃圾 | 删除无意义的东西，注释，文件，代码段等。" },
-			{ value: "⏩ mark-progress", name: "⏩ mark-progress:    标记进度 | 标记进度。" },
-		],
-		useEmoji: true,
-		emojiAlign: "center",
-		useAI: false,
-		aiNumber: 1,
-		themeColorCode: "",
 
-		upperCaseSubject: false,
-		markBreakingChangeMode: false,
-		allowBreakingChanges: ["feat", "fix"],
-		breaklineNumber: 100,
-		breaklineChar: "|",
-		skipQuestions: [],
-		issuePrefixes: [
-			// 如果使用 gitee 作为开发管理
-			{ value: "link", name: "link:     链接 ISSUES 进行中" },
-			{ value: "closed", name: "closed:   标记 ISSUES 已完成" },
-		],
-		customIssuePrefixAlign: "top",
-		emptyIssuePrefixAlias: "skip",
-		customIssuePrefixAlias: "custom",
-		allowCustomIssuePrefix: true,
-		allowEmptyIssuePrefix: true,
-		confirmColorize: true,
-		scopeOverrides: undefined,
-		defaultBody: "",
-		defaultIssues: "",
-		defaultScope: "",
-		defaultSubject: "",
-	},
-};
+			/**
+			 * 基于monorepo内项目，决定提交范围域
+			 */
+			scopes: scopes,
+
+			// https://cz-git.qbb.sh/zh/recipes/#多选模式
+			enableMultipleScopes: true,
+			scopeEnumSeparator: ",",
+
+			allowCustomScopes: true,
+			allowEmptyScopes: true,
+			customScopesAlign: "bottom",
+			customScopesAlias: "custom",
+			emptyScopesAlias: "empty",
+
+			types: [
+				{ value: "✨ feat", name: "✨ feat:     新增功能 | A new feature" },
+				{ value: "🐞 fix", name: "🐞 fix:      修复缺陷 | A bug fix" },
+				{ value: "📃 docs", name: "📃 docs:     文档更新 | Documentation only changes" },
+				{ value: "🌈 style", name: "🌈 style:    代码格式 | Changes that do not affect the meaning of the code" },
+				{
+					value: "🦄 refactor",
+					name: "🦄 refactor: 代码重构 | A code change that neither fixes a bug nor adds a feature",
+				},
+				{ value: "🎈 perf", name: "🎈 perf:     性能提升 | A code change that improves performance" },
+				{ value: "🧪 test", name: "🧪 test:     测试相关 | Adding missing tests or correcting existing tests" },
+				{
+					value: "🔧 build",
+					name: "🔧 build:    构建相关 | Changes that affect the build system or external dependencies",
+				},
+				{ value: "🐎 ci", name: "🐎 ci:       持续集成 | Changes to our CI configuration files and scripts" },
+				{ value: "↩ revert", name: "↩ revert:   回退代码 | Revert to a commit" },
+				{ value: "🐳 chore", name: "🐳 chore:    其他修改 | Other changes that do not modify src or test files" },
+				{
+					value: "🤔 save-file",
+					name: "🤔 save-file:    保存文件 | 文件保存类型。仅仅是为了保存文件。有时候会需要紧急提交，并快速切换分支。此时就需要提交代码。并保存文件。",
+				},
+				{ value: "⚙️ config", name: "⚙️ config:    更新配置 | 配置更新。通用性的配置更新。" },
+				{ value: "✋ main-pull-update", name: "✋ main-pull-update:    主分支拉取更新 | 主分支拉取更新。" },
+				{ value: "🗑 del", name: "🗑 del:    删除垃圾 | 删除无意义的东西，注释，文件，代码段等。" },
+				{ value: "⏩ mark-progress", name: "⏩ mark-progress:    标记进度 | 标记进度。" },
+			],
+			useEmoji: true,
+			emojiAlign: "center",
+			useAI: false,
+			aiNumber: 1,
+			themeColorCode: "",
+
+			upperCaseSubject: false,
+			markBreakingChangeMode: false,
+			allowBreakingChanges: ["feat", "fix"],
+			breaklineNumber: 100,
+			breaklineChar: "|",
+			skipQuestions: [],
+			issuePrefixes: [
+				// 如果使用 gitee 作为开发管理
+				{ value: "link", name: "link:     链接 ISSUES 进行中" },
+				{ value: "closed", name: "closed:   标记 ISSUES 已完成" },
+			],
+			customIssuePrefixAlign: "top",
+			emptyIssuePrefixAlias: "skip",
+			customIssuePrefixAlias: "custom",
+			allowCustomIssuePrefix: true,
+			allowEmptyIssuePrefix: true,
+			confirmColorize: true,
+			scopeOverrides: undefined,
+			defaultBody: "",
+			defaultIssues: "",
+			defaultScope: "",
+			defaultSubject: "",
+		},
+	};
+})();
