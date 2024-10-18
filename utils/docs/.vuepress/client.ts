4f17625449e2f44d87140a3d51ef3ca8f5f90e00
ruan-cat <1219043956@qq.com>
2024-10-18 10:44:14 +0800

    ⚙️ config(utils): 移除不需要的样式文件；使用普通样式，暂时不使用小爱丽丝样式。

--- utils/docs/.vuepress/client.ts
+++ utils/docs/.vuepress/client.ts
@@ -1,4 +1,4 @@
 // 测试 普通样式
-// import "@ruan-cat/vuepress-preset-config/src/styles/common/index.scss";
+import "@ruan-cat/vuepress-preset-config/src/styles/common/index.scss";
 // 测试 爱丽丝样式
-import "@ruan-cat/vuepress-preset-config/src/styles/drill-style/index.scss";
+// import "@ruan-cat/vuepress-preset-config/src/styles/drill-style/index.scss";
