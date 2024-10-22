f5f92e637f8beaa33aa3adf040462e4dbef9b32c
ruan-cat <1219043956@qq.com>
2024-10-22 11:49:32 +0800

    ✨ feat(vite-plugin-autogeneration-import-file-test-app-1): 完成类型生成、删除、与前置打包等行为。

--- vite-plugin-autogeneration-import-file/app/scripts/del-types.ts
+++ vite-plugin-autogeneration-import-file/app/scripts/del-types.ts
@@ -5,29 +5,9 @@ import { consola } from "consola";
 
 import { dirname, resolve } from "node:path";
 
-/**
- * 拼接路径
- * @private
- * 不作为公共方法 与特定路径强耦合的
- *
- * @deprecated
- * 不需要 对应的删除函数不需要生成具体的路径
- */
-function joinPath(dir: string): string {
-	const resPath = resolve("..", "types", dir);
-	// console.log(" in joinPath => ", resPath);
-	return resPath;
-}
-
-/**
- * TODO: 拆分成各个小配置字符串，移植到vite配置内，作为静态常量。
- */
 const filenames = <const>[
-	//
-	// "components-instance.d.ts",
-	// "components.d.ts",
 	// 填写glob语法 对于 deleteAsync 函数匹配有效。
-	"components*.d.ts",
+	"generate-types*.d.ts",
 	"typed-router.d.ts",
 	"auto-imports.d.ts",
 ];
--- vite-plugin-autogeneration-import-file/app/vite.config.ts
+++ vite-plugin-autogeneration-import-file/app/vite.config.ts
@@ -1,11 +1,28 @@
+import { dirname, resolve } from "node:path";
+import { fileURLToPath, URL } from "node:url";
+import * as fs from "node:fs";
+
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import Components from "unplugin-vue-components/vite";
 import { getName, createPlugin } from "@ruan-cat-test/vite-plugin-autogeneration-import-file";
+
 const { autoImport, resolver } = createPlugin();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+function pathResolve(dir: string) {
+	const resPath = resolve(__dirname, ".", dir);
+	return resPath;
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	server: {
+		port: 8080,
+		open: true,
+	},
+
 	plugins: [
 		vue(),
 
@@ -13,8 +30,8 @@ export default defineConfig({
 			// components
 			{
 				pattern: ["**/{index.vue,index.ts,index.js}", "*.{vue,ts,js}"],
-				dir: "./src/components",
-				toFile: "./components1.d.ts",
+				dir: pathResolve("./src/components"),
+				toFile: pathResolve("./types/generate-types-components.d.ts"),
 				template: `
 import '@vue/runtime-core'
 export {}
@@ -28,21 +45,22 @@ declare module '@vue/runtime-core' {
 			},
 
 			// module
-			// {
-			// 	pattern: ["**/*.{ts,js}", "*.{ts,js}"],
-			// 	dir: "./src/store/modules",
-			// 	toFile: "./src/store/module.ts",
-			// 	name: (name) => {
-			// 		name = getName(name);
-			// 		return name[0].toUpperCase() + name.slice(1) + "Store";
-			// 	},
-			// },
+			{
+				pattern: ["**/*.{ts,js}", "*.{ts,js}"],
+				// dir: pathResolve("./src/store/modules"),
+				dir: pathResolve("./src/store"),
+				toFile: pathResolve("./types/generate-types-store-modules.d.ts"),
+				name: (name) => {
+					name = getName(name);
+					return name[0].toUpperCase() + name.slice(1) + "Store";
+				},
+			},
 
 			// myComponents
 			{
 				pattern: ["**/{index.vue,index.ts,index.js}", "*.{vue,ts,js}"],
-				dir: "./src/myComponents",
-				toFile: "./myComponents.d.ts",
+				dir: pathResolve("./src/myComponents"),
+				toFile: pathResolve("./types/generate-types-myComponents.d.ts"),
 				template: `
 import '@vue/runtime-core'
 export {}
@@ -58,8 +76,8 @@ declare module '@vue/runtime-core' {
 			// myDirective
 			{
 				pattern: ["**/{index.vue,index.ts,index.js}", "*.{vue,ts,js}"],
-				dir: "./src/myDirective",
-				toFile: "./myDirective.d.ts",
+				dir: pathResolve("./src/myDirective"),
+				toFile: pathResolve("./types/generate-types-myDirective.d.ts"),
 				template: `
 import '@vue/runtime-core'
 export {}
