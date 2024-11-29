e1fcd12e76e7e585c61d90c43d0cac28537e86e6
ruan-cat <1219043956@qq.com>
2024-11-30 00:03:29 +0800

    🎈 perf(monorepo-1): 优化更加简短的项目部署路径。精简项目目录。

---
+++ tests/monorepo-1/src/configs/vp-hope/config.ts
@@ -0,0 +1,18 @@
+import { defineRuanCatVuepressConfig } from "@ruan-cat/vuepress-preset-config";
+
+/**
+ * 路径
+ * @description
+ * 待优化 找到一个方式，实现动态获取当前路径
+ */
+const currPath = "src/configs/vp-hope";
+
+export default defineRuanCatVuepressConfig({
+	title: "monorepo-1",
+	base: "/vp-hope/",
+	// dest: `${currPath}/dist`,
+	dest: `.vercel/output/static/vp-hope`,
+	cache: `${currPath}/.cache`,
+	temp: `${currPath}/.temp`,
+	public: `${currPath}/public`,
+});
