eb2102893cd176141b3e54c8e17a81676d34a7fa
ruan-cat <1219043956@qq.com>
2024-12-28 01:53:30 +0800

    🐞 fix(utils): 在node内运行axios，需要屏蔽掉代理，避免请求失败。

--- packages/utils/src/tests/mall-apifox-port/simple-axios.test.ts
+++ packages/utils/src/tests/mall-apifox-port/simple-axios.test.ts
@@ -10,32 +10,30 @@ import axios from "axios";
 function createAxiosInstance() {
 	const instance = axios.create({
 		baseURL: "https://pcapi-xiaotuxian-front-devtest.itheima.net",
-
 		/** 请求超时时间 */
 		timeout: 10000,
-
 		/** 允许跨域 */
 		// withCredentials: true,
+
+		/**
+		 * 在node内运行axios，需要屏蔽掉代理，避免请求失败。
+		 * @see https://github.com/axios/axios/issues/925
+		 * @see https://stackoverflow.com/questions/77258713/400-the-plain-http-request-was-sent-to-https-port-in-node-js
+		 */
+		proxy: false,
 	});
-
-	// 使用qs序列化参数params参数
-	// instance.defaults.paramsSerializer = function (params) {
-	// 	return qs.stringify(params);
-	// };
-
 	return instance;
 }
 
 function main() {
-	axios
-		.get(
-			//
-			//
-			"https://pcapi-xiaotuxian-front-devtest.itheima.net/home/category/head",
-			// "https://pcapi-xiaotuxian-front-devtest.itheima.net/home/hot"
-		)
+	const instance = createAxiosInstance();
+	instance
+		.get("/home/category/head")
 		.then((res) => {
-			console.log(" ? ", res);
+			console.log(" ? ", res.data);
+		})
+		.catch((error) => {
+			console.error("Request failed", error);
 		});
 }
 
