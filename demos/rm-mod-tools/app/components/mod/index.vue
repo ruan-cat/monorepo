b40bc53c105d3b136c9e2ccedd180c029a7da29b
ruan-cat <1219043956@qq.com>
2025-01-04 03:45:34 +0800

    🐳 chore(rm-mod-tools): 改写代码

--- demos/rm-mod-tools/app/components/mod/index.vue
+++ demos/rm-mod-tools/app/components/mod/index.vue
@@ -1,32 +1,29 @@
 <script lang="ts" setup>
-const app = {
-	data() {
-		return {
-			message: "MOD管理器",
-			isSidebarExpanded: false,
-			menuItems: ["首页", "设置", "关于", "帮助"],
-			listData: [],
-			isModalOpen: false, // 控制 Modal 显示与否
-			newCard: {
-				name: "",
-				img: "",
-				cmd: "",
-			},
-		};
-	},
-	mounted() {
-		loadCards("C:\\Users\\QwQ\\Documents\\插件测试", this);
-		loadCards("E:\\RPGMV\\【已整理】\\莉可的不可思议差事\\莉可的不可思议差事 Ver 1.41", this);
-		loadCards("E:\\RPGMV\\【已整理】\\Mary↑GO→LAND!!\\Mary↑GO→LAND!! Ver1.10\\www", this);
-		loadCards(
-			"E:\\RPGMV\\【已整理】\\Memories Story～囚われの者たち～\\Memories Story～囚われの者たち～ Ver1.01",
-			this,
-		);
-		loadCards("E:\\RPGMV\\【已整理】\\あの夏の島\\あの夏の島_Ver1.01", this);
-		loadCards("E:\\RPGMV\\【未处理】\\240302\\www", this);
+// import { spawn } from "node:child_process";
 
-		window.addEventListener("keydown", this.handleF5);
-	},
+const message = ref("MOD管理器");
+const isSidebarExpanded = ref(false);
+const menuItems = ref(["首页", "设置", "关于", "帮助"]);
+const listData = ref([]);
+const isModalOpen = ref(false);
+const newCard = ref({
+	name: "",
+	img: "",
+	cmd: "",
+});
+
+onMounted(() => {
+	loadCards("C:\\Users\\QwQ\\Documents\\插件测试", app);
+	loadCards("E:\\RPGMV\\【已整理】\\莉可的不可思议差事\\莉可的不可思议差事 Ver 1.41", app);
+	loadCards("E:\\RPGMV\\【已整理】\\Mary↑GO→LAND!!\\Mary↑GO→LAND!! Ver1.10\\www", app);
+	loadCards("E:\\RPGMV\\【已整理】\\Memories Story～囚われの者たち～\\Memories Story～囚われの者たち～ Ver1.01", app);
+	loadCards("E:\\RPGMV\\【已整理】\\あの夏の島\\あの夏の島_Ver1.01", app);
+	loadCards("E:\\RPGMV\\【未处理】\\240302\\www", app);
+	// TODO: 更改函数
+	window.addEventListener("keydown", app.methods.handleF5);
+});
+
+const app = {
 	methods: {
 		updateMessage() {
 			this.message = "消息已更新！";
@@ -43,21 +40,7 @@ const app = {
 		handleCardClick(card, index) {
 			console.log(`卡片点击: ${card.name}, 索引: ${index}`);
 		},
-		handleCardDblClick(card, index) {
-			const { spawn } = require("child_process");
-			const exec = require("child_process").exec;
-			const path = require("path");
-			var nwjsPath = "E:\\RM_RPG\\【道果】\\nw\\nw.exe";
-			var cmdPath = "E:\\RM_RPG\\【道果】\\nw\\app";
-			var appPath = cmdPath + "\\app副本";
-			var jsPath = cmdPath + "\\PC\\www\\js\\";
-			console.log("启动项目:", card.cmd);
-			var project = spawn(nwjsPath, ["."], {
-				cwd: appPath,
-				detached: true,
-				env: { Project_Path: card.cmd, jsPath: jsPath },
-			});
-		},
+
 		showDeleteButton(card, index) {
 			if (this.activeDeleteIndex !== index) {
 				this.activeDeleteIndex = index;
@@ -69,20 +52,24 @@ const app = {
 				card.showDelete = false;
 			}
 		},
+
 		deleteCard(index) {
 			this.listData.splice(index, 1);
 			this.activeDeleteIndex = null;
 		},
+
 		// 提交卡片数据并关闭弹窗
 		submitCard() {
 			this.listData.push(this.newCard);
 			this.isModalOpen = false; // 关闭弹窗
 			console.log("新卡片已添加:", this.newCard);
 		},
+
 		// 关闭弹窗
 		closeModal() {
 			this.isModalOpen = false;
 		},
+
 		// Trigger File Input
 		triggerFileInput() {
 			const input = document.createElement("input");
@@ -96,6 +83,7 @@ const app = {
 			};
 			input.click();
 		},
+
 		getImageUrl(card) {
 			if (card.key) {
 				if (card.engine == "RMMV") {
@@ -103,6 +91,7 @@ const app = {
 				}
 			} else return card.img;
 		},
+
 		handleF5(event) {
 			if (event.key === "F5") {
 				event.preventDefault();
@@ -114,16 +103,19 @@ const app = {
 		openModal() {
 			this.isModalOpen = true;
 		},
+
 		closeModal() {
 			this.isModalOpen = false;
 			// 重置新卡片数据
 			this.newCard = { name: "", img: "", cmd: "" };
 		},
+
 		addCard() {
 			// 将新的卡片数据添加到 listData
 			this.listData.push({ ...this.newCard, showDelete: false });
 			this.closeModal();
 		},
+
 		handleImageUpload(event) {
 			const file = event.target.files[0];
 			if (file) {
@@ -135,7 +127,25 @@ const app = {
 				reader.readAsDataURL(file); // 将图片读取为数据URL
 			}
 		},
+
+		// TODO: 用库重构成合适的文件读取
+		handleCardDblClick(card, index) {
+			// const { spawn } = require("child_process");
+			// const exec = require("child_process").exec;
+			// const path = require("path");
+			// var nwjsPath = "E:\\RM_RPG\\【道果】\\nw\\nw.exe";
+			// var cmdPath = "E:\\RM_RPG\\【道果】\\nw\\app";
+			// var appPath = cmdPath + "\\app副本";
+			// var jsPath = cmdPath + "\\PC\\www\\js\\";
+			// console.log("启动项目:", card.cmd);
+			// var project = spawn(nwjsPath, ["."], {
+			// 	cwd: appPath,
+			// 	detached: true,
+			// 	env: { Project_Path: card.cmd, jsPath: jsPath },
+			// });
+		},
 	},
+
 	beforeUnmount() {
 		window.removeEventListener("keydown", this.handleF5);
 	},
