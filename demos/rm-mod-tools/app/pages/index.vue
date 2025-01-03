484b744242bdc5e7aa4c89f7a9c9ffd6b0f23dbb
ruan-cat <1219043956@qq.com>
2025-01-04 03:06:53 +0800

    ⚙️ config: 配置prettier格式化的多选忽略文件。格式化部分文件。

--- demos/rm-mod-tools/app/pages/index.vue
+++ demos/rm-mod-tools/app/pages/index.vue
@@ -1,32 +1,30 @@
 <script setup lang="ts">
 definePageMeta({
-  layout: 'home',
-})
+	layout: "home",
+});
 
-const online = useOnline()
+const online = useOnline();
 </script>
 
 <template>
-  <div>
-    <Logos mb-6 />
-    <ClientOnly>
-      <Suspense>
-        <PageView v-if="online" />
-        <div v-else text-gray:80>
-          You're offline
-        </div>
-        <template #fallback>
-          <div italic op50>
-            <span animate-pulse>Loading...</span>
-          </div>
-        </template>
-      </Suspense>
-      <template #fallback>
-        <div op50>
-          <span animate-pulse>...</span>
-        </div>
-      </template>
-    </ClientOnly>
-    <InputEntry />
-  </div>
+	<div>
+		<Logos mb-6 />
+		<ClientOnly>
+			<Suspense>
+				<PageView v-if="online" />
+				<div v-else text-gray:80>You're offline</div>
+				<template #fallback>
+					<div italic op50>
+						<span animate-pulse>Loading...</span>
+					</div>
+				</template>
+			</Suspense>
+			<template #fallback>
+				<div op50>
+					<span animate-pulse>...</span>
+				</div>
+			</template>
+		</ClientOnly>
+		<InputEntry />
+	</div>
 </template>
