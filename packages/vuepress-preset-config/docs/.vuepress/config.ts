f2a9bdeebf70b6290da4ee8df76213482ecb692b
ruan-cat <1219043956@qq.com>
2025-01-31 14:32:44 +0800

    🧪 test(vuepress-preset-config): 测试导入配置文件后，能否完成渲染。可以完成正常的渲染

--- packages/vuepress-preset-config/docs/.vuepress/config.ts
+++ packages/vuepress-preset-config/docs/.vuepress/config.ts
@@ -1,13 +1,17 @@
-import { defineUserConfig } from "vuepress";
+// import { defineUserConfig } from "vuepress";
+// import theme from "./theme.js";
+// export default defineUserConfig({
+// 	base: "/",
+// 	lang: "zh-CN",
+// 	title: "文档演示",
+// 	description: "vuepress-theme-hope 的文档演示",
+// 	theme,
+// });
 
-import theme from "./theme.js";
-
-export default defineUserConfig({
-	base: "/",
-
+// import { defineRuanCatVuepressConfig } from "../../src/index.ts";
+import { defineRuanCatVuepressConfig } from "@ruan-cat/vuepress-preset-config";
+export default defineRuanCatVuepressConfig({
 	lang: "zh-CN",
-	title: "文档演示",
-	description: "vuepress-theme-hope 的文档演示",
-
-	theme,
+	title: "这里是预设配置的测试文档",
+	description: "测试文档",
 });
--- packages/vuepress-preset-config/docs/.vuepress/theme.ts
+++ packages/vuepress-preset-config/docs/.vuepress/theme.ts
@@ -1,37 +1,15 @@
 import { hopeTheme } from "vuepress-theme-hope";
 
-import navbar from "./navbar.js";
-
 export default hopeTheme({
 	hostname: "https://vuepress-theme-hope-docs-demo.netlify.app",
-
-	author: {
-		name: "Mr.Hope",
-		url: "https://mister-hope.com",
-	},
-
-	logo: "https://theme-hope-assets.vuejs.press/logo.svg",
-
 	docsDir: "docs",
-
-	// 导航栏
-	navbar,
-
 	// 侧边栏
 	sidebar: "structure",
-
 	// 页脚
 	footer: "默认页脚",
 	displayFooter: true,
-
-	// 多语言配置
-	metaLocales: {
-		editLink: "在 GitHub 上编辑此页",
-	},
-
 	// 如果想要实时查看任何改变，启用它。注: 这对更新性能有很大负面影响
 	hotReload: true,
-
 	// 此处开启了很多功能用于演示，你应仅保留用到的功能。
 	markdown: {
 		align: true,
@@ -53,13 +31,11 @@ export default hopeTheme({
 		tasklist: true,
 		vPre: true,
 	},
-
 	// 在这里配置主题提供的插件
 	plugins: {
 		components: {
 			components: ["Badge", "VPCard"],
 		},
-
 		icon: {
 			prefix: "fa6-solid:",
 		},
