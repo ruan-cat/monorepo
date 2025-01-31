314fc465751903961c590174a49970e107438397
ruan-cat <1219043956@qq.com>
2025-01-31 19:59:05 +0800

    🗑 del: 删除掉两个nuxt相关的项目，删除掉多余的demo项目

--- demos/main-app/src/index.ts
+++
@@ -1,8 +0,0 @@
-import { add } from "../../../tests/add-tool/src";
-
-// Example usage of the add-tool functionality
-const result = add(2, 3);
-console.log(result);
-
-// Main application logic
-console.log("Hello, world!");
--- demos/rm-mod-tools/app/constants/index.ts
+++
@@ -1,3 +0,0 @@
-// export const appName = "Vitesse for Nuxt 3";
-export const appName = "MOD管理器";
-export const appDescription = "Vitesse for Nuxt 3";
