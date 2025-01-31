e9f804c22b0c561fe5e2caef53a72d816c6ee205
ruan-cat <1219043956@qq.com>
2025-01-31 15:12:16 +0800

    🐳 chore(vuepress-preset-config,monorepo-4): 疑似符号链接出现问题，直接导入纯净的js文件可以正常运行项目。

--- tests/monorepo-4/src/.vuepress/config.ts
+++ tests/monorepo-4/src/.vuepress/config.ts
@@ -1,4 +1,6 @@
 import { defineRuanCatVuepressConfig } from "@ruan-cat/vuepress-preset-config";
+// @ts-ignore
+// import { defineRuanCatVuepressConfig } from "../../../../packages/vuepress-preset-config/dist/vuepress-preset-config.js";
 export default defineRuanCatVuepressConfig({
 	lang: "zh-CN",
 	title: "这里是 monorepo-4",
