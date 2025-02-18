b7d86f816c731311d692c4aa750f5e2d2eb7a873
ruan-cat <1219043956@qq.com>
2025-02-18 14:57:51 +0800

    ✨ feat(utils): 生成简单的执行命令函数

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,6 +1,5 @@
 export * from "./rmmv-class-expand-tools.ts";
 export * from "./conditions.ts";
-export * from "./Prettify.ts";
 export * from "./unplugin-vue-router/index.ts";
 export * from "./define-promise-tasks.ts";
 export * from "./simple-promise-tools.ts";
@@ -9,3 +8,4 @@ export * from "./ruan-cat-pkg-info.ts";
 export * from "./node.ts";
 
 export * from "./types/pnpm-workspace.yaml.shim.ts";
+export * from "./types/Prettify.ts";
--- packages/utils/src/node.ts
+++ packages/utils/src/node.ts
@@ -3,6 +3,11 @@
  */
 
 import { normalize } from "node:path";
+import { spawnSync } from "node:child_process";
+
+import { generateSimpleAsyncTask } from "@ruan-cat/utils";
+import gradient from "gradient-string";
+import consola from "consola";
 // import { normalizePath } from "vite";
 
 /**
@@ -14,4 +19,64 @@ export function pathChange(path: string) {
 	// return normalize(path);
 	// FIXME: tsup打包时，无法处理好vite的依赖 会导致打包失败 不知道怎么单独使用并打包该函数
 	// return normalizePath(path);
+}
+
+export interface SpawnSyncSimpleParams {
+	command: string;
+	parameters: string[];
+	/**
+	 * 是否流式输出内容
+	 * @description 默认输出的命令数据全部以流式的方式输出
+	 * @default true
+	 */
+	isFlow?: boolean;
+
+	/**
+	 * 是否显示命令？
+	 * @description
+	 * 是否打印目前正在执行的命令？
+	 * @default true
+	 */
+	isShowCommand: boolean;
+}
+
+/**
+ * 生成简单的执行命令函数
+ * @description
+ * 对 spawnSync 做简单的包装
+ *
+ * 之前封装的是 execa 函数
+ * @version 2
+ */
+export function generateSpawnSync(spawnSyncSimpleParams: SpawnSyncSimpleParams) {
+	const { command, parameters, isFlow = true, isShowCommand = true } = spawnSyncSimpleParams;
+
+	if (isShowCommand) {
+		const coloredCommand = gradient(["rgb(0, 153, 247)", "rgb(241, 23, 18)"])(`${command} ${parameters.join(" ")}`);
+		consola.info(` 当前运行的命令为： ${coloredCommand} \n`);
+	}
+
+	return generateSimpleAsyncTask(() => {
+		const result = spawnSync(command, parameters, {
+			/**
+			 * 是否流式输出？
+			 * 是流式输出就是继承父进程的流式输出
+			 * 否则就使用默认值
+			 * @see https://nodejs.org/api/child_process.html#optionsstdio
+			 */
+			stdio: isFlow ? "inherit" : "pipe",
+			shell: true,
+		});
+
+		// 如果不是流式输出 就直接返回返回值即可
+		if (!isFlow) {
+			return result;
+		}
+
+		if (result.error) {
+			throw result.error;
+		}
+
+		return result;
+	});
 }
