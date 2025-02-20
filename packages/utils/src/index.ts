3232ec45fc82e9ea7a831bf330af894a95a026c3
ruan-cat <1219043956@qq.com>
2025-02-20 12:36:25 +0800

    🦄 refactor(utils): 移动目标的位置。

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -3,7 +3,7 @@ export * from "./conditions.ts";
 export * from "./unplugin-vue-router/index.ts";
 export * from "./define-promise-tasks.ts";
 export * from "./simple-promise-tools.ts";
-export * from "./vueuse/useAxios.ts";
+export * from "./vueuse/useAxios/index.ts";
 export * from "./ruan-cat-pkg-info.ts";
 
 export * from "./types/pnpm-workspace.yaml.shim.ts";
