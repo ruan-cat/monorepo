b770ba07f58dce5ee446b8dc6650b54b3e977f89
ruan-cat <1219043956@qq.com>
2025-03-13 19:33:46 +0800

    🐞 fix(utils): 处理 homeCategoryHead.ts 的类型约束报错。

--- packages/utils/src/vueuse/useAxios/tests/homeCategoryHead.ts
+++ packages/utils/src/vueuse/useAxios/tests/homeCategoryHead.ts
@@ -1,20 +1,23 @@
-import { projectRequest } from "./projectRequest.ts";
+import { projectRequest, type UseAxiosOptionsWithReturnWrapper } from "./projectRequest.ts";
 import { HomeCategoryHeads, HomeCategoryHeadsParams } from "./types/business.ts";
-import type { KeyHelper } from "@ruan-cat/utils/vueuse";
 
 /**
  * 具体的业务接口
  * @see https://apifox.com/apidoc/shared-c05cb8d7-e591-4d9c-aff8-11065a0ec1de/api-67132163
  */
-export function homeCategoryHead<T>() {
+export function homeCategoryHead<T = HomeCategoryHeads[]>(options: UseAxiosOptionsWithReturnWrapper<T>) {
 	/**
 	 * 定义接口的dto返回值
 	 * 接口传参时使用的字段
 	 * 传参时传递的vo对象
 	 */
-	return projectRequest<"method" | "url" | "data", HomeCategoryHeads[], HomeCategoryHeadsParams>({
+	return projectRequest<"method" | "data", T, HomeCategoryHeadsParams>({
+		options,
+		url: "/home/category/head",
 		config: {
-			url: "/home/category/head",
+			data: {
+				vip: true,
+			},
 			method: "get",
 		},
 	});
