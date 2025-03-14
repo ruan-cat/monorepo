2a6f291bb7afcb2deb44c61e96c1bc96a1347283
ruan-cat <1219043956@qq.com>
2025-03-15 02:27:46 +0800

    🎈 perf(utils): 删除多余的变更日志处理任务。

--- packages/utils/docs/.vuepress/config.ts
+++ packages/utils/docs/.vuepress/config.ts
@@ -1,5 +1,17 @@
 import { defineRuanCatVuepressConfig } from "@ruan-cat/vuepress-preset-config";
 
+import { addChangelog2doc } from "@ruan-cat/utils/node-esm";
+
+addChangelog2doc({
+	target: "./docs",
+	data: {
+		order: 940,
+		dir: {
+			order: 940,
+		},
+	},
+});
+
 export default defineRuanCatVuepressConfig({
 	title: "阮喵喵工具包",
 });
--- packages/utils/scripts/yaml-in-md.ts
+++
@@ -1,9 +0,0 @@
-import { writeYaml2md } from "../../../scripts/yaml-in-md.ts";
-writeYaml2md({
-	data: {
-		order: 100,
-		dir: {
-			order: 100,
-		},
-	},
-});
