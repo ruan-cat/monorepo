859119161c00f3502af00c862b49f04041585c59
ruan-cat <1219043956@qq.com>
2025-03-14 20:34:36 +0800

    🧪 test(vitepress-preset-config): 测试内部属性合并是否成功

--- packages/vitepress-preset-config/src/tests/merge-config.test.ts
+++ packages/vitepress-preset-config/src/tests/merge-config.test.ts
@@ -1,11 +1,16 @@
 import { describe, it, expect } from "vitest";
 
-import { setGenerateSidebar, setUserConfig } from "@ruan-cat/vitepress-preset-config/config.mts";
+import defConfig, { setGenerateSidebar, setUserConfig } from "@ruan-cat/vitepress-preset-config/config.mts";
 
 describe("测试合并配置", () => {
 	it("setUserConfig 设置vitepress用户配置", () => {
 		const testTitle = "测试标题";
-		const resTitle = setUserConfig({ title: testTitle }).title;
+		const resConfig = setUserConfig({ title: testTitle });
+		const resTitle = resConfig.title;
+
 		expect(resTitle).toBe(testTitle);
+		expect(resConfig.vite?.server?.open).toBe(defConfig.vite?.server?.open);
+
+		console.log("  setUserConfig 设置vitepress用户配置", resConfig);
 	});
 });
