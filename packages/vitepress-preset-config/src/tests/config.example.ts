dbd5a299d3b0e8602577ff4c2828ecda2e05a482
ruan-cat <1219043956@qq.com>
2025-03-15 18:22:42 +0800

    📃 docs(vitepress-preset-config): 更新更加有代表性的简单案例。

--- packages/vitepress-preset-config/src/tests/config.example.ts
+++ packages/vitepress-preset-config/src/tests/config.example.ts
@@ -16,20 +16,11 @@ addChangelog2doc({
 const userConfig = setUserConfig({
 	title: "阮喵喵的01星球笔记",
 	description: "阮喵喵在01星球内的一些公用文档",
-	lang: "zh",
 	themeConfig: {
-		nav: [
-			{ text: "首页", link: "/" },
-			{ text: "仓库地址", link: "https://github.com/ruan-cat/vercel-monorepo-test/tree/dev/packages/docs-01-star" },
-		],
-		outline: {
-			label: "本页目录",
-			level: "deep",
-		},
 		socialLinks: [
 			{
 				icon: "github",
-				link: "https://github.com/ruan-cat/vercel-monorepo-test/blob/dev/packages/docs-01-star/docs/index.md",
+				link: "https://github.com/ruan-cat",
 			},
 		],
 	},
