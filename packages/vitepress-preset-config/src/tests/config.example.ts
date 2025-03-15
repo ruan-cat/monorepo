b6a861efbfcaa93824d708893e3e621657b82a97
ruan-cat <1219043956@qq.com>
2025-03-15 14:09:41 +0800

    📃 docs(vitepress-preset-config): 补全 addChangelog2doc 的使用案例。

--- packages/vitepress-preset-config/src/tests/config.example.ts
+++ packages/vitepress-preset-config/src/tests/config.example.ts
@@ -1,4 +1,17 @@
-import { setUserConfig, setGenerateSidebar } from "@ruan-cat/vitepress-preset-config/config";
+import { setUserConfig, setGenerateSidebar, addChangelog2doc } from "@ruan-cat/vitepress-preset-config/config";
+
+// 为文档添加自动生成的changelog
+addChangelog2doc({
+	// 设置changelog的目标文件夹
+	target: "./docs",
+	// 设置changelog顶部的yaml数据。通常是排序
+	data: {
+		order: 1000,
+		dir: {
+			order: 1000,
+		},
+	},
+});
 
 const userConfig = setUserConfig({
 	title: "阮喵喵的01星球笔记",
