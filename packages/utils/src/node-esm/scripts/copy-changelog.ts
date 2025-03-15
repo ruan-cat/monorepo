c1afc30f261ed675fe2139a7c60c9b716a82c1fe
ruan-cat <1219043956@qq.com>
2025-03-15 14:39:15 +0800

    🐞 fix(utils): 处理 hasChangelogMd 提示错误的bug。

--- packages/utils/src/node-esm/scripts/copy-changelog.ts
+++ packages/utils/src/node-esm/scripts/copy-changelog.ts
@@ -4,9 +4,12 @@ import consola from "consola";
 
 /** 检查当前运行的根目录 是否存在 CHANGELOG.md 文件 */
 export function hasChangelogMd() {
-	consola.log("当前项目根目录为：", process.cwd());
-	consola.warn("当前项目根目录不存在 CHANGELOG.md 文件");
-	return fs.existsSync(path.resolve(process.cwd(), "CHANGELOG.md"));
+	const res = fs.existsSync(path.resolve(process.cwd(), "CHANGELOG.md"));
+	if (!res) {
+		consola.log("当前项目根目录为：", process.cwd());
+		consola.warn("当前项目根目录不存在 CHANGELOG.md 文件");
+	}
+	return res;
 }
 
 /**
