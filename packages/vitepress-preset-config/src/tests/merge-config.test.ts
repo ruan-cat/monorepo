dea3fbf122e1f94b1e15df5fb13114a40d8e00da
ruan-cat <1219043956@qq.com>
2025-03-15 14:34:21 +0800

    🐞 fix(vitepress-preset-config): 更新测试用例的报错。

--- packages/vitepress-preset-config/src/tests/merge-config.test.ts
+++ packages/vitepress-preset-config/src/tests/merge-config.test.ts
@@ -1,6 +1,6 @@
 import { describe, it, expect } from "vitest";
 
-import defConfig, { setGenerateSidebar, setUserConfig } from "@ruan-cat/vitepress-preset-config/config";
+import { setGenerateSidebar, setUserConfig } from "@ruan-cat/vitepress-preset-config/config";
 
 describe("测试合并配置", () => {
 	it("setUserConfig 设置vitepress用户配置", () => {
@@ -9,7 +9,7 @@ describe("测试合并配置", () => {
 		const resTitle = resConfig.title;
 
 		expect(resTitle).toBe(testTitle);
-		expect(resConfig.vite?.server?.open).toBe(defConfig.vite?.server?.open);
+		expect(resConfig.vite?.server?.open).toBe(true);
 
 		console.log("  setUserConfig 设置vitepress用户配置", resConfig);
 	});
