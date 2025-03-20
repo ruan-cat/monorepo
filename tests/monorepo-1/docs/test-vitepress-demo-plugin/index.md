4a11ccabbc1c6d66c5a893eaf7d8fc782bd623fd
ruan-cat <1219043956@qq.com>
2025-03-20 20:48:06 +0800

    🧪 test(monorepo-1): 增加针对 @ruan-cat/vitepress-preset-config 的测试用例。

---
+++ tests/monorepo-1/docs/test-vitepress-demo-plugin/index.md
@@ -0,0 +1,3 @@
+# 测试 `@ruan-cat/vitepress-preset-config` 是否能直接显示 vue 组件？
+
+<demo vue="./index.vue" />
---
+++ tests/monorepo-1/docs/test-vitepress-demo-plugin/index.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+import { ref, computed, watch } from "vue";
+</script>
+
+<template>
+	<section class="index-root">
+		<h1>是否能直接显示 vue 组件</h1>
+	</section>
+</template>
+
+<style lang="scss" scoped>
+.index-root {
+}
+</style>
