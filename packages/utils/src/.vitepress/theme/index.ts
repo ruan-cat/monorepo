19ae55711d5dd608f8fbfaa76da4ebd3311d61f4
ruan-cat <1219043956@qq.com>
2025-03-22 23:06:33 +0800

    ⚙️ config: 更新多个文档项目的主题配置；

--- packages/utils/src/.vitepress/theme/index.ts
+++ packages/utils/src/.vitepress/theme/index.ts
@@ -1,3 +1,4 @@
 import { defineRuancatPresetTheme } from "@ruan-cat/vitepress-preset-config/theme";
+import "@ruan-cat/vitepress-preset-config/theme.css";
 import "./style.css";
 export default defineRuancatPresetTheme();
--- packages/vitepress-preset-config/src/.vitepress/theme/index.ts
+++ packages/vitepress-preset-config/src/.vitepress/theme/index.ts
@@ -1,5 +1,6 @@
 import { defineRuancatPresetTheme } from "../../theme";
 
+import "@ruan-cat/vitepress-preset-config/theme.css";
 import "./style.css";
 
 export default defineRuancatPresetTheme();
