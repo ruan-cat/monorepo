b90d3e59be42f56a78a66bf7ec1a479bdbec6e58
ruan-cat <1219043956@qq.com>
2025-03-23 01:06:12 +0800

    ✨ feat(vuepress-preset-config): 重构配置。要求外部主题配置自己组装配置

--- packages/vitepress-preset-config/src/theme.ts
+++ packages/vitepress-preset-config/src/theme.ts
@@ -21,27 +21,36 @@ import {
 	type Options,
 	LayoutMode,
 } from "@nolebase/vitepress-plugin-enhanced-readabilities/client";
-import "@nolebase/vitepress-plugin-enhanced-readabilities/client/style.css";
+// import "@nolebase/vitepress-plugin-enhanced-readabilities/client/style.css";
 
 import { NolebaseHighlightTargetedHeading } from "@nolebase/vitepress-plugin-highlight-targeted-heading/client";
-import "@nolebase/vitepress-plugin-highlight-targeted-heading/client/style.css";
+// import "@nolebase/vitepress-plugin-highlight-targeted-heading/client/style.css";
 
 import { NolebaseGitChangelogPlugin } from "@nolebase/vitepress-plugin-git-changelog/client";
-import "@nolebase/vitepress-plugin-git-changelog/client/style.css";
+// import "@nolebase/vitepress-plugin-git-changelog/client/style.css";
 
 import TwoslashFloatingVue from "@shikijs/vitepress-twoslash/client";
-import "@shikijs/vitepress-twoslash/style.css";
+// import "@shikijs/vitepress-twoslash/style.css";
 
-/** 一个回调函数 用来暴露变量 实现注册 */
+/**
+ * 一个回调函数 用来暴露变量 实现注册\
+ * @deprecated
+ */
 export interface EnhanceAppCallBack {
 	({ app, router, siteData }: EnhanceAppContext): void;
 }
 
+/**
+ * @deprecated
+ */
 export interface DefineRuancatPresetThemeParams {
 	enhanceAppCallBack?: EnhanceAppCallBack;
 }
 
-/** 定义默认主题预设 */
+/**
+ * 定义默认主题预设
+ * @deprecated
+ */
 export function defineRuancatPresetTheme(params?: DefineRuancatPresetThemeParams) {
 	return {
 		extends: DefaultTheme,
@@ -81,3 +90,47 @@ export function defineRuancatPresetTheme(params?: DefineRuancatPresetThemeParams
 		},
 	} satisfies Theme;
 }
+
+/** 默认布局配置 */
+const defaultLayoutConfig = {
+	// https://vitepress.dev/guide/extending-default-theme#layout-slots
+	"doc-before": () => h(NolebaseBreadcrumbs),
+	"nav-bar-content-after": () => h(NolebaseEnhancedReadabilitiesMenu),
+	// 为较窄的屏幕（通常是小于 iPad Mini）添加阅读增强菜单
+	"nav-screen-content-after": () => h(NolebaseEnhancedReadabilitiesScreenMenu),
+	"layout-top": () => [h(NolebaseHighlightTargetedHeading)],
+};
+
+/** 默认 enhanceApp 预设 */
+export function defaultEnhanceAppPreset({ app, router, siteData }: EnhanceAppContext) {
+	app.use(NolebaseGitChangelogPlugin);
+	app.use(TwoslashFloatingVue);
+	app.provide(InjectionKey, {
+		layoutSwitch: {
+			defaultMode: LayoutMode["BothWidthAdjustable"],
+			pageLayoutMaxWidth: {
+				defaultMaxWidth: 85,
+			},
+			contentLayoutMaxWidth: {
+				defaultMaxWidth: 95,
+			},
+		},
+	} as Options);
+}
+
+/** 默认主题配置 */
+export const defaultTheme = {
+	extends: DefaultTheme,
+	Layout: () => {
+		return h(DefaultTheme.Layout, null, defaultLayoutConfig);
+	},
+	enhanceApp({ app, router, siteData }: EnhanceAppContext) {
+		defaultEnhanceAppPreset({ app, router, siteData });
+		/**
+		 * 放弃全局注册demo展示组件
+		 * 在生产环境内使用peer对等依赖
+		 */
+		// app.component("VitepressDemoBox", VitepressDemoBox);
+		// app.component("VitepressDemoPlaceholder", VitepressDemoPlaceholder);
+	},
+} satisfies Theme;
