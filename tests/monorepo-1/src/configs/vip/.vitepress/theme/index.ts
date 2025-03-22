1f4a26ab013e3004314f34fa4fce7ad5916b24be
ruan-cat <1219043956@qq.com>
2025-03-23 01:16:34 +0800

    ⚙️ config: 更新主题配置；

--- tests/monorepo-1/src/configs/vip/.vitepress/theme/index.ts
+++ tests/monorepo-1/src/configs/vip/.vitepress/theme/index.ts
@@ -1,4 +1,18 @@
-import { defineRuancatPresetTheme } from "@ruan-cat/vitepress-preset-config/theme";
+// https://vitepress.dev/guide/custom-theme
+import { h } from "vue";
+import type { Theme, EnhanceAppContext } from "vitepress";
+import DefaultTheme from "vitepress/theme";
+import { defaultLayoutConfig, defaultEnhanceAppPreset } from "@ruan-cat/vitepress-preset-config/theme";
+
+// 导入全部的主题样式
 import "@ruan-cat/vitepress-preset-config/theme.css";
 import "./style.css";
-export default defineRuancatPresetTheme();
+export default {
+	extends: DefaultTheme,
+	Layout: () => {
+		return h(DefaultTheme.Layout, null, defaultLayoutConfig);
+	},
+	enhanceApp({ app, router, siteData }: EnhanceAppContext) {
+		defaultEnhanceAppPreset({ app, router, siteData });
+	},
+} satisfies Theme;
