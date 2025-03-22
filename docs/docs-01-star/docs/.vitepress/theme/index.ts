66faa4bbfe3a2139b9a3cb7f410c98335c75b8fd
ruan-cat <1219043956@qq.com>
2025-03-22 20:39:19 +0800

    ⚙️ config(docs-01-star)!: 更新主题配置。

--- docs/docs-01-star/docs/.vitepress/theme/index.ts
+++ docs/docs-01-star/docs/.vitepress/theme/index.ts
@@ -1,35 +1,3 @@
-// https://vitepress.dev/guide/custom-theme
-// import { h } from "vue";
-// import type { Theme } from "vitepress";
-// import DefaultTheme from "vitepress/theme";
-// import "./style.css";
-// import { NolebaseBreadcrumbs } from "@nolebase/vitepress-plugin-breadcrumbs/client";
-// import {
-// 	NolebaseEnhancedReadabilitiesMenu,
-// 	NolebaseEnhancedReadabilitiesScreenMenu,
-// } from "@nolebase/vitepress-plugin-enhanced-readabilities/client";
-// import "@nolebase/vitepress-plugin-enhanced-readabilities/client/style.css";
-// import { NolebaseHighlightTargetedHeading } from "@nolebase/vitepress-plugin-highlight-targeted-heading/client";
-// import "@nolebase/vitepress-plugin-highlight-targeted-heading/client/style.css";
-// import { NolebaseGitChangelogPlugin } from "@nolebase/vitepress-plugin-git-changelog/client";
-// import "@nolebase/vitepress-plugin-git-changelog/client/style.css";
-// export default {
-// 	extends: DefaultTheme,
-// 	Layout: () => {
-// 		return h(DefaultTheme.Layout, null, {
-// 			// https://vitepress.dev/guide/extending-default-theme#layout-slots
-// 			"doc-before": () => h(NolebaseBreadcrumbs),
-// 			"nav-bar-content-after": () => h(NolebaseEnhancedReadabilitiesMenu),
-// 			// 为较窄的屏幕（通常是小于 iPad Mini）添加阅读增强菜单
-// 			"nav-screen-content-after": () => h(NolebaseEnhancedReadabilitiesScreenMenu),
-// 			"layout-top": () => [h(NolebaseHighlightTargetedHeading)],
-// 		});
-// 	},
-// 	enhanceApp({ app, router, siteData }) {
-// 		// ...
-// 		app.use(NolebaseGitChangelogPlugin);
-// 	},
-// } satisfies Theme;
-
-import theme from "@ruan-cat/vitepress-preset-config/theme";
-export default theme;
+import { defineRuancatPresetTheme } from "@ruan-cat/vitepress-preset-config/theme";
+import "./style.css";
+export default defineRuancatPresetTheme();
--- packages/vitepress-preset-config/src/tests/theme.example.ts
+++ packages/vitepress-preset-config/src/tests/theme.example.ts
@@ -1,2 +1,5 @@
 import { defineRuancatPresetTheme } from "@ruan-cat/vitepress-preset-config/theme";
+// @ts-ignore 记得增加样式
+import "./style.css";
+
 export default defineRuancatPresetTheme();
