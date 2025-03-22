1e1d6f1e1238ad87ecf27ec7eee8a64407556ad9
ruan-cat <1219043956@qq.com>
2025-03-22 23:00:42 +0800

    📃 docs(vitepress-preset-config): 导入全部的主题样式

--- packages/vitepress-preset-config/src/tests/theme.example.ts
+++ packages/vitepress-preset-config/src/tests/theme.example.ts
@@ -1,4 +1,8 @@
 import { defineRuancatPresetTheme } from "@ruan-cat/vitepress-preset-config/theme";
+
+// 导入全部的主题样式
+import "@ruan-cat/vitepress-preset-config/theme.css";
+
 // @ts-ignore 记得增加样式
 import "./style.css";
 
