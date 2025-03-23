6acd9fcfce2e65285850dd63b5392515996f932e
ruan-cat <1219043956@qq.com>
2025-03-23 19:51:43 +0800

    📃 docs(vitepress-preset-config)!: 更新主题使用案例。

--- packages/vitepress-preset-config/src/tests/theme.example.ts
+++ packages/vitepress-preset-config/src/tests/theme.example.ts
@@ -1,21 +1,9 @@
-// https://vitepress.dev/guide/custom-theme
-import { h } from "vue";
-import type { Theme, EnhanceAppContext } from "vitepress";
-import DefaultTheme from "vitepress/theme";
-import { defaultLayoutConfig, defaultEnhanceAppPreset } from "@ruan-cat/vitepress-preset-config/theme";
+import { defineRuancatPresetTheme } from "@ruan-cat/vitepress-preset-config/src/theme.ts";
 
 // 导入全部的主题样式
 import "@ruan-cat/vitepress-preset-config/theme.css";
 
-// @ts-ignore 记得增加样式
+// 增加用户自定义样式
 import "./style.css";
 
-export default {
-	extends: DefaultTheme,
-	Layout: () => {
-		return h(DefaultTheme.Layout, null, defaultLayoutConfig);
-	},
-	enhanceApp({ app, router, siteData }: EnhanceAppContext) {
-		defaultEnhanceAppPreset({ app, router, siteData });
-	},
-} satisfies Theme;
+export default defineRuancatPresetTheme();
