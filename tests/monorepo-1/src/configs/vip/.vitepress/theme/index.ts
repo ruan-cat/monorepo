6794933c388285f4d2f55ae227736d003fe64d6c
ruan-cat <1219043956@qq.com>
2025-03-23 20:51:19 +0800

    🐳 chore: 移除对外导出的样式配置。

--- tests/monorepo-1/src/configs/vip/.vitepress/theme/index.ts
+++ tests/monorepo-1/src/configs/vip/.vitepress/theme/index.ts
@@ -1,8 +1,5 @@
 import { defineRuancatPresetTheme } from "@ruan-cat/vitepress-preset-config/theme";
 
-// 导入全部的主题样式
-import "@ruan-cat/vitepress-preset-config/theme.css";
-
 // 增加用户自定义样式
 import "./style.css";
 
