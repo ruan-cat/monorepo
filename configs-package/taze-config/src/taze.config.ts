a9bea5b75f8e0e16787520b675ced2def4343c52
ruan-cat <1219043956@qq.com>
2025-07-17 03:06:02 +0800

    ✨ feat(taze-config): 新增默认配置，包含包管理和版本控制选项。

---
+++ configs-package/taze-config/src/taze.config.ts
@@ -0,0 +1,38 @@
+import { defineConfig } from "taze";
+
+export const defaultConfig: Parameters<typeof defineConfig>["0"] = {
+	// fetch latest package info from registry without cache
+	force: true,
+
+	// write to package.json
+	write: true,
+
+	// run `npm install` or `yarn install` right after bumping
+	/**
+	 * 不主动执行安装依赖的行为 升级版本号 但是不升级
+	 * 要求用户随后主动运行安装依赖的命令。
+	 */
+	install: false,
+
+	// ignore paths for looking for package.json in monorepo
+	ignorePaths: ["**/node_modules/**", "**/test/**"],
+
+	// ignore package.json that in other workspaces (with their own .git,pnpm-workspace.yaml,etc.)
+	ignoreOtherWorkspaces: true,
+
+	// override with different bumping mode for each package
+	packageMode: {
+		codemirror: "ignore",
+		cropperjs: "ignore",
+		vite: "ignore",
+		// regex starts and ends with '/'
+		"/@ruan-cat/": "latest",
+		"/@form-create/": "next",
+		"/@wangeditor/": "next",
+	},
+
+	// disable checking for "overrides" package.json field
+	depFields: {
+		overrides: false,
+	},
+};
