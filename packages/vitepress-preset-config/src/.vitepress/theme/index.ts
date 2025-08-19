c8e00cd6231b1ce0efc97f2501010d2f35e118c1
ruan-cat <1219043956@qq.com>
2025-08-19 13:25:32 +0800

    🗑 del(vitepress-preset-config): 移除冗余的垃圾。删除掉测试用的配置文件。

--- packages/vitepress-preset-config/src/.vitepress/theme/index.ts
+++ packages/vitepress-preset-config/src/.vitepress/theme/index.ts
@@ -1,26 +1,7 @@
 import { defineRuancatPresetTheme } from "@ruan-cat/vitepress-preset-config/theme";
 import type { EnhanceAppContext } from "vitepress";
 
-// export default defineRuancatPresetTheme();
-
 // 增加用户自定义样式
 import "./style.css";
-
-import Teek from "vitepress-theme-teek";
-import "vitepress-theme-teek/index.css";
-
-import { NolebaseGitChangelogPlugin } from "@nolebase/vitepress-plugin-git-changelog/client";
-import "@nolebase/vitepress-plugin-git-changelog/client/style.css";
-
-import TwoslashFloatingVue from "@shikijs/vitepress-twoslash/client";
-import "@shikijs/vitepress-twoslash/style.css";
-
-// export default {
-// 	extends: Teek,
-// 	enhanceApp({ app, router, siteData }: EnhanceAppContext) {
-// 		app.use(NolebaseGitChangelogPlugin);
-// 		app.use(TwoslashFloatingVue);
-// 	},
-// };
 
 export default defineRuancatPresetTheme();
