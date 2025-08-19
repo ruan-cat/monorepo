a8436aabf34c578147628f7a0abfbc939969093d
ruan-cat <1219043956@qq.com>
2025-08-19 19:23:12 +0800

    ✨ feat(vitepress-preset-config)!: 恢复 defineRuancatPresetTheme 的回调能力，让外部可以根据情况，使用 enhanceApp 来自定义注册组件。

--- packages/vitepress-preset-config/src/theme.ts
+++ packages/vitepress-preset-config/src/theme.ts
@@ -18,14 +18,56 @@ import Teek from "vitepress-theme-teek";
 import "vitepress-theme-teek/index.css";
 
 /**
+ * 一个回调函数 用来暴露变量 实现注册
+ */
+export interface EnhanceAppCallBack {
+	({ app, router, siteData }: EnhanceAppContext): void;
+}
+
+/**
+ */
+export interface DefineRuancatPresetThemeParams {
+	enhanceAppCallBack?: EnhanceAppCallBack;
+}
+
+/**
+ * 默认 enhanceApp 预设
+ * @description
+ * 这个函数预期应该作为一个内部函数 不应该对外暴露使用
+ * @private
+ */
+function defaultEnhanceAppPreset({ app, router, siteData }: EnhanceAppContext) {
+	app.use(NolebaseGitChangelogPlugin);
+	app.use(TwoslashFloatingVue);
+	/**
+	 * 放弃全局注册demo展示组件
+	 * 在生产环境内使用peer对等依赖
+	 */
+	// app.component("VitepressDemoBox", VitepressDemoBox);
+	// app.component("VitepressDemoPlaceholder", VitepressDemoPlaceholder);
+}
+
+/** 默认主题配置 */
+export const defaultTheme = {
+	extends: Teek,
+	Layout: Teek.Layout,
+	enhanceApp({ app, router, siteData }: EnhanceAppContext) {
+		defaultEnhanceAppPreset({ app, router, siteData });
+	},
+} satisfies Theme;
+
+/**
  * 定义默认主题预设
  */
-export function defineRuancatPresetTheme() {
+export function defineRuancatPresetTheme(params?: DefineRuancatPresetThemeParams) {
 	return {
-		extends: Teek,
+		...defaultTheme,
 		enhanceApp({ app, router, siteData }: EnhanceAppContext) {
-			app.use(NolebaseGitChangelogPlugin);
-			app.use(TwoslashFloatingVue);
+			// 回调默认主题内的函数
+			defaultTheme.enhanceApp({ app, router, siteData });
+			// 执行用户传入的回调函数
+			params?.enhanceAppCallBack?.({ app, router, siteData });
 		},
+		// setup() {},
 	} satisfies Theme;
 }
