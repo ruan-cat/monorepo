087db2abcfe47da0edbaafc142d369520923dc58
ruan-cat <1219043956@qq.com>
2025-08-20 18:36:40 +0800

    ✨ feat(release-toolkit)!: 配置 changelogen.config.ts 不需要参与tsup打包，更改该配置的使用方式。

--- .config/changelogen.config.ts
+++ .config/changelogen.config.ts
@@ -3,6 +3,6 @@
  * 使用 @ruan-cat/release-toolkit 提供的预设配置
  */
 
-import config from "@ruan-cat/release-toolkit/configs/changelogen.config";
+import config from "@ruan-cat/release-toolkit/src/configs/changelogen.config.ts";
 
 export default config;
--- packages/release-toolkit/tsup.config.ts
+++ packages/release-toolkit/tsup.config.ts
@@ -3,7 +3,7 @@ import { defineConfig, type Options } from "tsup";
 export default defineConfig((options: Options) => [
 	// 主入口文件和所有模块一起构建
 	{
-		entry: ["./src/index.ts", "./src/plugins/changelog-with-changelogen.ts", "./src/configs/changelogen.config.ts"],
+		entry: ["./src/index.ts", "./src/plugins/changelog-with-changelogen.ts"],
 		sourcemap: true,
 		outDir: "dist",
 		format: ["esm", "cjs"],
