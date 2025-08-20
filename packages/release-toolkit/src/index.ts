efa2e193743a6a313c871f48aeaa4a951badf1f2
ruan-cat <1219043956@qq.com>
2025-08-20 18:07:39 +0800

    🐞 fix(release-toolkit): 处理文件导入尾缀的错误。

--- packages/release-toolkit/src/index.ts
+++ packages/release-toolkit/src/index.ts
@@ -6,15 +6,15 @@
  */
 
 // 导出 changesets 插件
-export { default as changelogFunctions } from "./plugins/changelog-with-changelogen";
-export * from "./plugins/changelog-with-changelogen";
+export { default as changelogFunctions } from "./plugins/changelog-with-changelogen.ts";
+export * from "./plugins/changelog-with-changelogen.ts";
 
 // 导出 GitHub Release 同步工具
-export { GitHubReleaseSync, runSync } from "./scripts/sync-github-release";
-export type { PublishedPackage, ChangelogEntry } from "./scripts/sync-github-release";
+export { GitHubReleaseSync, runSync } from "./scripts/sync-github-release.ts";
+export type { PublishedPackage, ChangelogEntry } from "./scripts/sync-github-release.ts";
 
 // 导出 changelogen 配置
-export { default as changelogConfig } from "./configs/changelogen.config";
+export { default as changelogConfig } from "./configs/changelogen.config.ts";
 
 // 重新导出 commitlint-config 的类型功能 (方便使用)
 export {
