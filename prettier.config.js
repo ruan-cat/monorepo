b8c586abaee3e568923ed629d5f016e55cc7be83
ruan-cat <1219043956@qq.com>
2025-08-22 16:16:17 +0800

    ⚙️ config(root)!: 开启prettier插件。

--- prettier.config.js
+++ prettier.config.js
@@ -2,7 +2,7 @@
 /** @type {import("prettier").Config} */
 const config = {
 	// FIXME: 该配置导致md文件格式化时收到干扰 配置不是热更新的 而且忽略配置是失效的
-	// plugins: ["prettier-plugin-lint-md"],
+	plugins: ["prettier-plugin-lint-md"],
 	singleQuote: false,
 	printWidth: 120,
 	semi: true,
@@ -10,10 +10,10 @@ const config = {
 	useTabs: true,
 	tabWidth: 2,
 	endOfLine: "auto",
-	// "space-around-alphabet": true,
-	// "space-around-number": true,
-	// "no-empty-code-lang": false,
-	// "no-empty-code": false,
+	"space-around-alphabet": true,
+	"space-around-number": true,
+	"no-empty-code-lang": false,
+	"no-empty-code": false,
 };
 
 export default config;
