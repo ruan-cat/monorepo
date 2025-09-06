725b06e42724811b6f2f49e94377648b22c2c3eb
ruan-cat <1219043956@qq.com>
2025-09-06 14:17:39 +0800

    🐳 chore(domains)!: 设置成动态加载的路径加载器；删除掉不需要的页面；标记注释说明。

--- packages/domains/docs/domain/[project].paths.ts
+++ packages/domains/docs/domain/[project].paths.ts
@@ -1,33 +1,12 @@
-// TODO: 更改成动态写法
-// import { projects } from "../../src/types";
+import { projects } from "../../src/types";
 
 export default {
 	paths() {
-		// const result = projects.map((project) => ({
-		// 	params: {
-		// 		project: project.name,
-		// 	},
-		// }));
-
-		// console.log(result);
-
-		const test = [
-			{ params: { project: "09oa" } },
-			{ params: { project: "10wms" } },
-			{ params: { project: "10wms-doc" } },
-			{ params: { project: "11comm" } },
-			{ params: { project: "11comm-doc" } },
-			{ params: { project: "01s-doc" } },
-			{ params: { project: "utils" } },
-			{ params: { project: "vitepress-preset" } },
-			{ params: { project: "domain" } },
-			{ params: { project: "vercel-deploy-tool" } },
-			{ params: { project: "ruan-cat-notes" } },
-			{ params: { project: "rmmv-notes" } },
-			{ params: { project: "drill-doc" } },
-			{ params: { project: "rmmv-api-doc" } },
-		];
-
-		return test;
+		const result = projects.map((project) => ({
+			params: {
+				project: project.name,
+			},
+		}));
+		return result;
 	},
 };
