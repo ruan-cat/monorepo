1c87e59437d1e2666b42d8bd401de6629527ac1b
ruan-cat <1219043956@qq.com>
2025-09-06 09:33:05 +0800

    ⚙️ config(vitepress-preset-config): 设计额外配置的插件类型。

--- packages/vitepress-preset-config/src/types.ts
+++ packages/vitepress-preset-config/src/types.ts
@@ -1,3 +1,10 @@
+import llmstxt from "vitepress-plugin-llms";
+import { GitChangelog, type GitChangelogMarkdownSectionOptions } from "@nolebase/vitepress-plugin-git-changelog/vite";
+
+type LlmstxtSettings = NonNullable<Parameters<typeof llmstxt>[0]>;
+
+type GitChangelogOptions = NonNullable<Parameters<typeof GitChangelog>[0]>;
+
 /**
  * 额外的配置
  * @description
@@ -9,5 +16,23 @@ export interface ExtraConfig {
 	 * @description
 	 * vitepress 配置的一系列插件
 	 */
-	plugins?: {};
+	plugins?: {
+		/**
+		 * @description 用于配置 vitepress-plugin-llms 插件
+		 * @see https://github.com/okineadev/vitepress-plugin-llms
+		 */
+		llmstxt?: LlmstxtSettings | false;
+
+		/**
+		 * @description 用于配置 @nolebase/vitepress-plugin-git-changelog 插件
+		 * @see
+		 */
+		gitChangelog?: GitChangelogOptions | false;
+
+		/**
+		 * @description 用于配置 @nolebase/vitepress-plugin-git-changelog-markdown-section 插件
+		 * @see
+		 */
+		gitChangelogMarkdownSection?: GitChangelogMarkdownSectionOptions | false;
+	};
 }
