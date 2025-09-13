0558ddace4a721e33a926ed0ec9f97e828a7dde9
ruan-cat <1219043956@qq.com>
2025-09-14 02:29:01 +0800

    ⚙️ config(root)!: 修改 simple-git-hooks.mjs 配置，使其可以主动实现 commitlint

--- simple-git-hooks.mjs
+++ simple-git-hooks.mjs
@@ -1,5 +1,6 @@
 export default {
-	// "commit-msg": "npx --no-install commitlint --edit '$1'",
+	/** @see https://juejin.cn/post/7381372081915166739#heading-8 */
+	"commit-msg": "pnpm commitlint",
 	"pre-commit": "npx lint-staged",
 	"pre-push": "pnpm run format",
 };
