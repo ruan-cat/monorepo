bcc918deea3a5e9a45de4cfdfa015c0531197d88
ruan-cat <1219043956@qq.com>
2025-10-28 15:47:50 +0800

    ✨ feat(vitepress-preset-config,root): 增加固定页面排序配置 pageOrderConfig 。

--- packages/vitepress-preset-config/src/config/index.ts
+++ packages/vitepress-preset-config/src/config/index.ts
@@ -1,5 +1,6 @@
 // 专门为 packages\vitepress-preset-config\src\config.mts 服务的文件 提供额外配置
 
+export * from "./page-order-config";
 export * from "./plugins";
 export * from "./changelog-nav";
 export * from "./teek";
